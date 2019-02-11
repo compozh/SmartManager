1ce5e208eaf21c3ea1f3d4117ccbb39cbad845ae
Мелентьев Евгений <Melentyev@it.ua>
2019-02-11 16:54:54 +0200

    Revert "Объединенный запрос на включение внесенных изменений 2: Revert "Merge branch 'master' of
    Revert "Merge branch 'master' of http://tfs2017:8080/tfs/DefaultCollection/_git/WebApplications""

--- SkdApplication/SkdApplication/ClientApp/app.js
+++ SkdApplication/SkdApplication/ClientApp/app.js
@@ -2,12 +2,10 @@ import Vue from 'vue'
 import axios from 'axios'
 import router from './router/index'
 import store from './store'
-import { sync } from 'vuex-router-sync'
+import {sync} from 'vuex-router-sync'
 import App from 'components/app-root'
-import { FontAwesomeIcon } from '@it-enterprise/fontawesome-icons'
 
-// Registration of global components
-Vue.component('icon', FontAwesomeIcon)
+
 
 Vue.prototype.$http = axios
 
@@ -15,13 +13,13 @@ Vue.prototype.$http = axios
 sync(store, router)
 
 const app = new Vue({
-  store,
-  router,
-  ...App
+	store,
+	router,
+	...App
 })
 
 export {
-  app,
-  router,
-  store
+	app,
+	router,
+	store
 }
---
+++ WebCommon/SkdApplication/ClientApp/components/userslist.vue
@@ -0,0 +1,53 @@
+<template>
+
+	<v-flex lg8 offset-lg2>
+		<v-flex md3 sm6 xl12>
+			<!-- <icon :icon="['fa', 'search']"/> -->
+			<v-text-field color="success" label="Поиск">
+			</v-text-field>
+		</v-flex>
+		<v-layout :key="userCom.USERID" v-for="userCom in users_list">
+			<list-component :userCopmonent='userCom'> <!-- передаю "привязываю" пользовательскому компоненту  данные  -->
+			</list-component>
+		</v-layout>
+	</v-flex>
+</template>
+
+<script>
+
+	import LicstComponent from "./listcomponent"
+	// import lodash from "lodash"
+	export default {
+		components: {
+			'list-component': LicstComponent // объявляю пользовательский компонент
+		},
+		data () {
+			return {}
+		},
+		computed: {
+			users_list () {
+				return this.$store.getters.getUsersList;//getters из vuex папка (store/index.js)
+			},
+			user () {
+				return this.$store.getters.getUser;//getters из vuex папка (store/index.js)
+			}
+		},
+		created: function () {
+		},
+		beforeMount: function () {
+			this.$store.dispatch('loadUsersList')
+			// //подробно разобрать beforeMount !!!!!!!!!!!!!!!!!!!!!!!!
+			// if(this.user=='wrong' || this.user==''){ // после рефреш появляется проблема с тем что, приходиться логинется, хотя состояние должно сохраниться
+			//    this.$router.push('login')
+			// }
+		},
+		methods: {}
+	}
+</script>
+
+<style>
+	.search {
+		background: #A5D6A7;
+	}
+
+</style>
