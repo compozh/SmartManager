ad55a30b93dcf303046e39be518ec40c816aaf49
Vitalii Bakhariev <baharev610@gmail.com>
2019-03-04 11:14:00 +0200

    api для health summary_health клиента

---
+++ ProjectSummaryHealthApi/ProjectSummaryHealthApi/Logic/HealthSummaryLogic.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using SkdLogic.Models;
+using Web.WebRequests;
+
+namespace ProjectSummaryHealthApi.Logic
+{
+	public class HealthSummaryLogic
+	{
+		public HealthSummaryLogic(WebRequestsTools webRequestsTools)
+		{
+			_webRequestsTools = webRequestsTools;
+		}
+		private readonly WebRequestsTools _webRequestsTools;
+
+		public async Task<string> getDetailInformationAsync(string criterion, string date, string parameters)
+		{
+			string calcId = "GET_PROJECT_HEALTH_DETAILS";
+			string args = "{\"CRITERION\":\"" + criterion + "\",\"DATESTR\":\"" + date + "\",\"PARAMETERS\":\"" + parameters + "\" }";
+
+			var task = _webRequestsTools.CallWebRequestAsync(calcId, args);
+			var requestResult = await task;
+			switch (requestResult.ResultFlag)
+			{
+				case WebRequestsResponseFlags.WrongTicket:
+				case WebRequestsResponseFlags.NotAuthorised:
+					return null;
+				default:
+					return requestResult.Content; ;
+			}
+		}
+		public async Task<string> getInfoAboutServerAsync(string date)
+		{
+			string calcId = "GET_PROJECT_HEALTH_SUMMARY";
+			string args = "{\"DATESTR\":\""+date+"\" }";
+
+			var task = _webRequestsTools.CallWebRequestAsync(calcId, args);
+			var requestResult = await task;
+			switch (requestResult.ResultFlag)
+			{
+				case WebRequestsResponseFlags.WrongTicket:
+				case WebRequestsResponseFlags.NotAuthorised:
+				return null;
+				default:
+				return requestResult.Content; ;
+			}
+		}
+	}
+
+}
