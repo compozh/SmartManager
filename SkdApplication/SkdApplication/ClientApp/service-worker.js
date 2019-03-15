d5eb79ef24a6905370a58843dc2d8fec2907dec9
Vitalii Bakhariev <baharev@it-enterprise.com>
2019-03-15 11:25:51 +0200

    рабочий вариант service-worker

--- SkdApplication/SkdApplication/ClientApp/service-worker.js
+++ SkdApplication/SkdApplication/ClientApp/service-worker.js
@@ -1,61 +1,76 @@
-var cacheName = 'SkdCach3';
-var dataCacheusersPhoto = 'usersPhoto3';
+var cacheName = 'SkdCach9';
+var dataCacheusersPhoto = 'usersPhoto9';
 var avalaible = [cacheName, dataCacheusersPhoto];
-var staticFiles=['/skd/',
-'/skd/dist/main.js',
-'/skd/dist/site.css',
-'/skd/dist/vendor.js',
-'/skd/dist/vendor-manifest.json',
-'https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css',
-'https://fonts.googleapis.com/css?family=Material+Icons',
-'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900%7CRoboto+Mono:500'
+var staticFiles = ['/skd/',
+	'/skd/dist/main.js',
+	'/skd/dist/site.css',
+	'/skd/dist/vendor.js',
+	'/skd/dist/vendor-manifest.json',
+	'https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css',
+	'https://fonts.googleapis.com/css?family=Material+Icons',
+	'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900%7CRoboto+Mono:500'
 ]
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(staticFiles);
-    })
-  );
+self.addEventListener('install', function (e) {
+	e.waitUntil(
+		caches.open(cacheName).then(function (cache) {
+			return cache.addAll(staticFiles);
+		})
+	);
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.filter(function(key) {
-          return avalaible.indexOf(key) == -1 
-        }).map(function(key){
-          console.log("cashe delated = "+ key)
-          return caches.delete(key)
-        }));
-    })
-  );
+self.addEventListener('activate', function (e) {
+	e.waitUntil(
+		caches.keys().then(function (keyList) {
+			return Promise.all(keyList.filter(function (key) {
+				return avalaible.indexOf(key) == -1
+			}).map(function (key) {
+				console.log("cashe delated = " + key)
+				return caches.delete(key)
+			}));
+		})
+	);
 });
 
-self.addEventListener('fetch', function(e) {
-  var dataUrl = 'https://m.it.ua/ws/GetFile.ashx';
-  var cacheName1 = ""
-  if (e.request.url.indexOf(dataUrl)>-1){
-    debugger
-    cacheName1=dataCacheusersPhoto
-  }
-    else if(staticFiles.filter(function(file){
-      return e.request.url.indexOf(file)>-1
-    })){
-      cacheName1=cacheName;
-    } 
-  if(e.request.url!="https://m.it.ua/skd/api/SkdApi/GetUsers"){
-  e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request).then(function(response){
-          caches.open(cacheName1).then(function(cache) {
-             cache.put(e.request.url, response.clone());
-              return response;
-          }); 
+self.addEventListener('fetch', function (e) {
+
+	if (!getCashName(e.request.url)) {
+		e.respondWith(
+			fetch(e.request)
+		);
+		return
+	}
+	e.respondWith(
+		caches.match(e.request).then(function (resp) {
+			return resp || fetch(e.request).then(function (response) {
+
+				caches.open(getCashName(e.request.url)).then(function (cache) {
+					if (getCashName(e.request.url)) {
+						console.log("cash put= ", e.request.url)
+						cache.put(e.request, response);
+					}
         });
-      })
-      .catch(function(){ e.respondWith(
-      fetch(e.request)
-    );})
-    );
-  }
+					return response.clone();
+			});
+		})
+	);
 });
+
+checkCash = function (url) {
+	 if(url.indexOf('/ws/GetFile.ashx')>-1)
+		console.log("users")
+	
+	return (staticFiles.some(function (file) {
+		if (url.indexOf(file) + file.length == url.length)
+			console.log(url, file)
+		return url.indexOf(file) + file.length == url.length
+	}))
+}
+
+getCashName = function (url) {
+	var dataUrl = '/ws/GetFile.ashx';
+	if (url.indexOf(dataUrl) > -1) {
+		return dataCacheusersPhoto
+	} else if (checkCash(url)) {
+		return cacheName;
+	}
+}
