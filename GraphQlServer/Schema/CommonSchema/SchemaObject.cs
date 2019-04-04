98240c23bb73ee24fb50235c98208d9a955606c7
Vitalii Bakhariev <baharev@it-enterprise.com>
2019-04-04 18:47:12 +0300

    [GraphQL]Вытягивание даных из схемы

---
+++ GraphQlServer/Schema/CommonSchema/SchemaObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace SkdScheme.CommonSchema
+{
+	public	class SchemaObject
+	{
+		public string Name { get; set; }
+		public string Id { get; set; }
+		public string Condition { get; set; }
+		public string Krep { get; set; }
+		public string Db { get; set; }
+		public List<SchemaColumn> Columns { get; set; }
+	}
+
+	public class SchemaColumn
+	{
+		public string Name { get; set; }
+		public string Expression { get; set; }
+		public string Description { get; set; }
+	}
+}
