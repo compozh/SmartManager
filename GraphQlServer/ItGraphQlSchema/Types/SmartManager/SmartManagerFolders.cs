ec47faf00b6d1258b1c2dace69ee2ca92d0d38e7
it\ostapenko.alexey <ostapenko.alexey@it-enterprise.com>
2019-04-26 17:51:33 +0300

    #615998 (Added Getting folders - via SM.GETFOLDERS)

---
+++ GraphQlServer/ItGraphQlSchema/Types/SmartManager/SmartManagerFolders.cs
@@ -0,0 +1,24 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItGraphQlSchema.Types.SmartManager
+{
+	
+	public class SmartManagerFolders : ObjectGraphType<SmFolders>, IItAddInSingleton
+	{
+		public SmartManagerFolders(SmFoldersProvider provider)
+		{
+			Name = "Folders";
+
+			Field(p => p.code).Description("Код папки");
+			Field(p => p.name).Description("Наименование");
+			Field(p => p.count).Description("Количество задач в папке");
+
+		}
+
+
+	}
+}
