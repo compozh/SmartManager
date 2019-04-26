0868ecfff6250d7ce1e398840882c49c9ddfbec1
Дмитрий Максимюк <maximuk.dmitriy@live.ru>
2019-04-26 10:04:36 +0300

    GraphQl Entities for purchase

---
+++ GraphQlServer/ItGraphQlSchema/Types/Purchases/Cart/CartItemType.cs
@@ -0,0 +1,44 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItGraphQlSchema.Types
+{
+	public class CartItemType : ObjectGraphType<CartItem>,IItAddInSingleton
+	{
+		public CartItemType(PurchasesProvider provider)
+		{
+			Name = "CartItem";
+			Field(p => p.Id).Description("Идентификатор элемента");
+			Field(p => p.ResourceId).Description("Идентификатор ресурса");
+			Field(p => p.ResourceName).Description("Наименование ресурса");
+			Field(p => p.Quantity).Description("Количество");
+			Field(p => p.MeasureUnit).Description("ЕИ");
+			Field(p => p.DateDelivery).Description("Плановая дата поставки");
+		}
+	}
+
+
+	public class CartTableType : ObjectGraphType,IItAddInSingleton
+	{
+		public CartTableType()
+		{
+			Name = "CartItemsTable";
+			Field<ListGraphType<CartItemType>>("data", description: "Данные с корзины");
+			Field<ListGraphType<CartItemColumnType>>("columns", description: "Перечень колонок");
+		}
+	}
+
+
+	public class CartItemColumnType : ObjectGraphType,IItAddInSingleton
+	{
+		public CartItemColumnType()
+		{
+			Name = " CartItemsColumns";
+			Field<StringGraphType>("key", "Ключ колонки");
+			Field<StringGraphType>("caption", "Заголовок колонки");
+		}
+
+	}
+}
---
+++ GraphQlServer/ItGraphQlSchema/Types/Purchases/PurchasesQuery.cs
@@ -0,0 +1,50 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.WebRequests;
+
+namespace ItGraphQlSchema.Types
+{
+	class Purchases : ObjectGraphType<object>, IItAddInSingleton
+	{
+
+		public Purchases(PurchasesProvider provider)
+		{
+			Name = "Query";
+
+			async Task<object> Resolve(ResolveFieldContext<object> context)
+			{
+				var cartItems = await provider.GetCartItems();
+
+				var subfields = context.SubFields["data"].SelectionSet.Children.Cast<GraphQL.Language.AST.Field>();//.Select(s => s.Name).ToList();
+
+				var columns = new CartItemType(provider).Fields;
+
+				var result = new
+				{
+					data = cartItems.ToList(),
+					columns = subfields.Select(s => {
+						var col = columns.FirstOrDefault(c => c.Name.Equals(s.Name, StringComparison.InvariantCultureIgnoreCase));
+						return col == null ? null : new { key = s.Name, caption = col.Description };
+					}).Where(f => f != null)
+				};
+
+				return result;
+			}
+
+
+			Field<CartTableType>("cartItemsTable",
+				arguments: new QueryArguments(),
+				resolve: (Resolve),
+				description: "Перечень позиций корзины");
+
+			Field<ListGraphType<CartItemType>>("cartItems",
+				arguments: new QueryArguments(),
+				resolve: (context => provider.GetCartItems()),
+				description: "Перечень позиций корзины");
+		}
+	}
+}
