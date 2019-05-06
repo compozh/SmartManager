07423489de6319518acd557485c1f646088413d0
it\ostapenko.alexey <ostapenko.alexey@it-enterprise.com>
2019-05-06 11:26:39 +0300

    #615998 добавлено  SM.GETTASKS   SM.TASK.GETINFO ( + вложенные подзарросы) ппки оставил как есть таски и инфо собрал в один провайдер

---
+++ GraphQlServer/ItGraphQlSchema/Types/SmartManager/SmartManagerFoldersQuery.cs
@@ -0,0 +1,22 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItGraphQlSchema.Types.SmartManager
+{
+	[AtributeAddInDI] 
+	public class SmartManagerFoldersQuery : ObjectGraphType<object>
+   {
+	  public SmartManagerFoldersQuery(SmFoldersProvider provider)
+		{
+			Name = "Query";
+			//получение папок
+			Field<ListGraphType<SmartManagerFolders>>("Folders",
+				resolve: (context => provider.getFoldersAsync()),
+				description: "Папки");
+		}
+	}
+}
---
+++ GraphQlServer/ItGraphQlSchema/Types/SmartManager/SmartManagerTasks.cs
@@ -0,0 +1,31 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace ItGraphQlSchema.Types.SmartManager
+{
+	[AtributeAddInDI]
+	public class SmartManagerTasks : ObjectGraphType<SmTasks>
+	{
+
+		public SmartManagerTasks(SmTasksProvider provider)
+		{
+			Name = "Tasks";
+			Field(p => p.name).Description("Название задачи");
+			Field(p => p.addedFio).Description("Имя");
+			//Field(p => p.HtmlDescription).Description("Html код");
+			Field(p => p.addedPhoto).Description("Фото");
+			Field(p => p.Id).Description("Идентификатор");
+			Field(p => p.childcount).Description("Количество");
+			Field(p => p.childdonecount).Description("Количество");
+			Field(p => p.dateadd).Description("Дата добавления");
+			Field(p => p.caseid).Description("Количество");
+
+		}
+
+	}
+
+}
