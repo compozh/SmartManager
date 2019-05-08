d61fcaf14dbb69ac3e6d73ad91d5fff3f8ccf78d
it\ostapenko.alexey <ostapenko.alexey@it-enterprise.com>
2019-05-08 14:32:05 +0300

    удаление ClientApp чтобы не потерять историю

--- WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/store/state.js
+++
@@ -1,8 +0,0 @@
-const state = {
-  applicationDescription:{},
-  appData: {},
-  // Текущий пользователь
-  currentuser: localStorage.getItem('userName')
-
-};
-export default state
--- WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  publicPath: "/",
-  //outputDir: "../WebAppBuilderApi/WebAppBuilderApi/dist"
- // outputDir: "../WebAppBuilderApi/ClientApp"
-
-}
