f1cc2283128fddf7b29d30596fbdfe7f07b1977f
it\ostapenko.alexey <ostapenko.alexey@it-enterprise.com>
2019-05-10 10:47:52 +0300

    #618566 измениене 2х файло из за изменения маршрута

--- WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/vue.config.js
+++ WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: "/webapps_api/",
-  outputDir: "../WebAppBuilderApi/WebAppBuilderApi/dist"
+  publicPath: "/",
+ // outputDir: "../WebAppBuilderApi/WebAppBuilderApi/dist"
 }
