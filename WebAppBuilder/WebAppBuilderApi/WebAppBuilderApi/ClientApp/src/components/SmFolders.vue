dcba5647a5b3b53acffa4a96a3c1cb68ad3bdb48
IT\Melentyev <melentyev@it.ua>
2019-05-13 17:49:02 +0300

    webapps. первый коммит с компонентами

---
+++ WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/components/SmFolders.vue
@@ -0,0 +1,44 @@
+<template >
+  <v-layout fill-height>
+    <v-flex xs3>
+      <v-list subheader class="sub">
+        <v-subheader inset>
+          <h2>Папки</h2>
+        </v-subheader>
+        <v-list-tile v-for="folder in folders" :key="folder.code" :to="{name:'SMARTMANAGERTASKS',     params:{foldercode:(folder.code ||'ALL')}}" active-class="sm_active-folder">
+          <v-list-tile-action>
+            <v-icon>folder</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <span>{{folder.name}}</span>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-flex>
+
+    <v-flex xs9 ml-4>
+      <router-view name="SmTaskFolder"/>
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+// var router= ({})
+export default {
+  name: "sm-folders",
+  data: () => ({
+    pagSync: {
+      rowsPerPage: -1
+    }
+  }),
+  props: ["folders"]
+};
+</script>
+<style >
+.sm_active-folder{
+  background:#ededed;
+}
+</style>
---
+++ WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/components/SmTasks.vue
@@ -0,0 +1,34 @@
+<template >
+  <div fill-height>
+    <h1>Задачи</h1>
+
+    <!-- {{tasks}}  -->
+    <v-flex>
+      <v-flex v-for="task in tasks" :key="task.id" pt-3>
+        <v-card pa-3>
+          <!-- <v-list-tile-action>
+                <v-icon>list</v-icon>
+          </v-list-tile-action>-->
+
+          <v-list-tile-content>
+            <v-list-tile-title>Идентификатор: {{task.id}}</v-list-tile-title>
+            <v-list-tile-title>Название задчи: {{task.name}}</v-list-tile-title>
+            <v-list-tile-title>Имя: {{task.addedFio}}</v-list-tile-title>
+            <v-list-tile-title>Дата добавления: {{task.dateadd}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-card>
+      </v-flex>
+    </v-flex>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "sm-tasks",
+
+  props: ["tasks"]
+};
+</script>
+<style>
+</style>
