44ba0f652f528f10cba364abf1cdb73f806d4b59
Vitalii Bakhariev <baharev@it-enterprise.com>
2019-05-16 17:08:45 +0300

    проба положить в сессию, уходит в бэклог

--- GraphQlServer/ItGraphQlSchema/Types/ITLogic/Model/UserAccount.cs
+++ GraphQlServer/ItGraphQlSchema/Types/ITLogic/Model/UserAccount.cs
@@ -2,20 +2,8 @@ using GraphQL.Types;
 
 namespace ItGraphQlSchema.Types.ITLogic.Model
 {
-    [AtributeAddInDI]
-    public class UserAccount : ObjectGraphType<UserAccount>
+    public class UserAccount 
     {
-        public UserAccount()
-        {
-            Name = "User";
-            Field(p => p.LastName);
-            Field(p => p.FirstName);
-            Field(p => p.Patronymic);
-            Field(p => p.Photo);
-            Field(p => p.Email);
-            Field(p => p.DateBirthday);
-        }
-
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string Patronymic { get; set; }
