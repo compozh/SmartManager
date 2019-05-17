18502173de7e055271d243a29c7220842534d9e2
Igor Pevchev <pevchev@it-enterprise.com>
2019-05-17 17:57:52 +0300

    update(ITMenu, ITModule): update markup and styles

--- WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/components/ITPortal/ITMenu.vue
+++ WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/components/ITPortal/ITMenu.vue
@@ -1,139 +1,177 @@
 <template>
-  <div v-if="menu" class="menu-container">
-    <VueLogo class="all-svg"/>
-    <div class="menu-list">
-      <v-treeview
-        :items="ItemsMenu"
-        item-key="codeMenu"
-        activatable
-        open-on-click
-        hoverable
-        indeterminate-icon
-        transition
-        class="menu"
-      >
-        <template slot="label" slot-scope="{ item }">
-          <div v-if="item.isFolder">
-            <svg class="svg">
-              <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#' + item.image"></use>
-            </svg>
-            <span class="menu-item-text" :title="item.name">{{ item.name }}</span>
-          </div>
-          <router-link
-            v-else
-            class="none-link"
-            :to="{ name : 'MODULECONTENT', params : LoadModuleContent(item) }"
-          >
-            <svg class="svg">
-              <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#' + item.image"></use>
-            </svg>
-            <span class="menu-item-text" :title="item.name">{{ item.name }}</span>
-          </router-link>
-        </template>
-      </v-treeview>
-    </div>
 
-    <div class="menu-content">
-      <!-- Костыль -->
-      <router-view name="module" v-if="this.$route.params.module"></router-view>
-      <Module v-if="!this.$route.params.module"/>
-    </div>
-  </div>
+  <v-container ma-0 pa-0 text-xs-left>
+    <it-icons class="all-svg"/>
+    <v-layout row class="layout">
+      <v-flex v-if="menu" xs12 md4 lg3 class="menu-list">
+        <v-treeview
+          class="treeview"
+          :items="menuItems"
+          item-key="codeMenu"
+          hoverable
+          open-on-click
+          indeterminate-icon
+        >
+
+          <template slot="label" slot-scope="{ item }">
+            <div v-if="item.isFolder" class="menu-item">
+              <svg class="svg">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#' + item.image"></use>
+              </svg>
+              <span class="menu-item-text" :title="item.name">{{ item.name }}</span>
+            </div>
+            <div v-else class="menu-item">
+              <router-link
+                class="none-link"
+                :to="{ name : 'MODULECONTENT', params : LoadModuleContent(item) }"
+              >
+                <svg class="svg">
+                  <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#' + item.image"></use>
+                </svg>
+                <span class="menu-item-text" :title="item.name">{{ item.name }}</span>
+              </router-link>
+            </div>
+          </template>
+        </v-treeview>
+      </v-flex>
+      <v-flex xs102 md8 lg9>
+        <v-layout>
+          <v-flex xs12>
+            <div class="menu-content">
+              <!-- Костыль -->
+              <router-view name="module" v-if="this.$route.params.module"></router-view>
+              <it-module v-if="!this.$route.params.module"/>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import Module from "./ITModule";
-import VueLogo from "../../svgfiles/ItIcons.svg";
-import getImage from "./ConvetImage.js";
+  import ItModule from "./ITModule";
+  import ItIcons from "../../svgfiles/ItIcons.svg";
+  import getImage from "./ConvetImage.js";
 
-export default {
-  name: "it-menu",
-  components: {
-    VueLogo,
-    Module
-  },
-  props: ["menu"],
-  computed: {
-    ItemsMenu() {
-      if (!this.SvgCollection.length) {
-        return [];
-      }
-      for (let object of this.menu.items) {
-        if (!object.children) {
-          object.children = [];
-        }
-        if (!object.image == "") {
-          object.image = object.image.toUpperCase();
-        } else {
-          object.image = "IT";
-        }
-        let inspaction = "";
-        for (let child of object.children) {
-          if (!child) {
-            break;
-          }
-
-          child.image = getImage(child.image, this.SvgCollection);
-        }
-      }
-      return this.menu.items;
+  export default {
+    name: "it-menu",
+    components: {
+      ItIcons,
+      ItModule
     },
-    SvgCollection() {
-      return this.$store.getters.getExistedIcons;
+    props: ["menu"],
+    computed: {
+      menuItems() {
+        if (!this.SvgCollection.length) {
+          return [];
+        }
+        for (let object of this.menu.items) {
+          if (!object.children) {
+            object.children = [];
+          }
+          if (!object.image == "") {
+            object.image = object.image.toUpperCase();
+          } else {
+            object.image = "IT";
+          }
+          let inspaction = "";
+          for (let child of object.children) {
+            if (!child) {
+              break;
+            }
+
+            child.image = getImage(child.image, this.SvgCollection);
+          }
+        }
+        return this.menu.items;
+      },
+      SvgCollection() {
+        return this.$store.getters.getExistedIcons;
+      }
+    },
+    methods: {
+      LoadModuleContent(item) {
+        return {module: item.codeMenu};
+      }
+    },
+    updated() {
+      if (this.$store.getters.getExistedIcons.length == 0) {
+        let arr = Array.from(
+          document.getElementsByClassName("all-svg")[0].children
+        ).map(r => r.id);
+        this.$store.commit("setExistedIcons", arr);
+      }
+    },
+    created() {
     }
-  },
-  methods: {
-    LoadModuleContent(item) {
-      return { module: item.codeMenu };
-    }
-  },
-  updated() {
-    if (this.$store.getters.getExistedIcons.length == 0) {
-      let arr = Array.from(
-        document.getElementsByClassName("all-svg")[0].children
-      ).map(r => r.id);
-      this.$store.commit("setExistedIcons", arr);
-    }
-  },
-  created() {}
-};
+  };
 </script>
 
 <style scoped>
-.menu-container {
-  display: flex;
-}
-.menu {
-  text-align: left;
-}
-.menu-list {
-  width: 500px;
-  overflow: hidden;
-  margin-right: 50px;
-}
-.menu-item-text {
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  width: 300px;
-}
-.menu-content {
-  display: block;
-  float: left;
-  width: 100%;
-}
-.none-link {
-  color: black;
-  text-decoration: none !important;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-}
-.svg {
-  width: 20px;
-  height: 20px;
-  margin-right: 5px;
-  fill: #757575;
-}
+  .layout {
+    height: 87vh;
+  }
+
+  .menu-list {
+    padding: 0;
+    overflow: hidden;
+    overflow-y: auto;
+    margin-right: 50px;
+  }
+
+  .menu-list::-webkit-scrollbar-track-piece {
+    width: 30px;
+  }
+
+  .treeview {
+    overflow: hidden;
+  }
+
+  .menu-item {
+    display: flex;
+    align-items: center;
+  }
+
+  .menu-item-text {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 250px;
+  }
+
+  .menu-content {
+    display: block;
+    float: left;
+    width: 100%;
+  }
+
+  .none-link {
+    color: black;
+    text-decoration: none !important;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  .svg {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+    fill: #757575;
+  }
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+  }
+
 </style>
--- WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/components/ITPortal/ITModule.vue
+++ WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/components/ITPortal/ITModule.vue
@@ -1,115 +1,223 @@
 <template>
-  <div>
-    <h3>{{Module.title}}</h3>
-    <v-treeview
-      :items="Module.paragraphItem"
-      item-key="codeMenu"
-      open-on-click
-      hoverable
-      indeterminate-icon
-      transition
-      class="menu"
-    >
-      <template slot="label" slot-scope="{ item }">
-        <span v-if="item.linkToRMD">
-          <svg class="svg">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#' + item.image"></use>
-          </svg>
-          <a target="_blank" :href="BaseUrl+item.linkToRMD">{{ item.name }}</a>
-          <!-- <router-link class="none-link" :to="{name: 'ITCLIENT', query:{licnk : BaseUrl + item.linkToRMD} }"> {{ item.name }} </router-link> -->
-        </span>
-        <span v-if="!item.linkToRMD">{{ item.name }}</span>
-      </template>
-    </v-treeview>
-  </div>
+  <v-container ma-0 pa-0>
+    <v-layout column>
+      <v-flex text-xs-center>
+        <h1 class="subheading text-uppercase font-weight-thin mb-3 pb-2">{{ getModule.title }}</h1>
+      </v-flex>
+      <v-flex>
+        <v-layout row class="sub-menu">
+          <v-flex xs4 class="columns column-1">
+            <v-layout column>
+              <v-flex
+                class="pa-3"
+                v-for="(node, index) in getModule.paragraphItem"
+                v-if="(index + 3) % 3 === 0"
+                :key="node.codeMenu"
+              >
+                <v-layout column>
+                  <v-flex>
+                    <h2 class="body-2 font-weight-bold text-uppercase ml-4 pb-1">{{ node.name }}</h2>
+                  </v-flex>
+                  <v-flex
+                    d-flex
+                    class="node-item"
+                    v-for="item in node.children"
+                  >
+                    <div class="icon">
+                      <svg class="svg">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#' + item.image"></use>
+                      </svg>
+                    </div>
+
+                    <div>{{ item.name }}</div>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+
+          <v-flex xs4 class="columns column-2">
+            <v-layout column>
+              <v-flex
+                class="pa-3"
+                v-for="(node, index) in getModule.paragraphItem"
+                v-if="(index + 3) % 3 === 1"
+                :key="node.codeMenu"
+              >
+                <v-layout column>
+                  <v-flex>
+                    <h2 class="body-2 font-weight-bold text-uppercase ml-4 pb-1">{{ node.name }}</h2>
+                  </v-flex>
+                  <v-flex
+                    d-flex
+                    class="node-item"
+                    v-for="item in node.children"
+                  >
+                    <div class="icon">
+                      <svg class="svg">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#' + item.image"></use>
+                      </svg>
+                    </div>
+
+                    <div>{{ item.name }}</div>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+
+          <v-flex xs4 class="columns column-3">
+            <v-layout column>
+              <v-flex
+                class="pa-3"
+                v-for="(node, index) in getModule.paragraphItem"
+                v-if="(index + 3) % 3 === 2"
+                :key="node.codeMenu"
+              >
+                <v-layout column>
+                  <v-flex>
+                    <h2 class="body-2 font-weight-bold text-uppercase ml-4 pb-1">{{ node.name }}</h2>
+                  </v-flex>
+                  <v-flex
+                    d-flex
+                    class="node-item"
+                    v-for="item in node.children"
+                  >
+                    <div class="icon">
+                      <svg class="svg">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#' + item.image"></use>
+                      </svg>
+                    </div>
+
+                    <div>{{ item.name }}</div>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+
+  <!--        <span v-if="item.linkToRMD">-->
+  <!--          <svg class="svg">-->
+  <!--            <use xmlns:xlink="http://www.w3.org/1999/xlink" :xlink:href="'#' + item.image"></use>-->
+  <!--          </svg>-->
+  <!--          <a target="_blank" :href="BaseUrl+item.linkToRMD">{{ item.name }}</a>-->
+  <!--          &lt;!&ndash; <router-link class="none-link" :to="{name: 'ITCLIENT', query:{licnk : BaseUrl + item.linkToRMD} }"> {{ item.name }} </router-link> &ndash;&gt;-->
+  <!--        </span>-->
+  <!--        <span v-if="!item.linkToRMD">{{ item.name }}</span>-->
+
 </template>
 
 <script>
-import getImae from "./ConvetImage.js";
-export default {
-  name: "it-module",
-  computed: {
-    Module: function() {
-      if (this.$store.getters.getAppData("ITMODULE")) {
-        if (!this.SvgCollection.length) {
-          return [];
-        }
-        var list = this.$store.getters.getAppData("ITMODULE").data.itmenu
-          .moduleContent;
+  import getImage from "./ConvetImage.js";
 
-        for (var object of list.paragraphItem) {
-          var inspaction = "";
-          object.children = object.children.filter(item => item.linkToRMD);
-          for (var child of object.children) {
-            if (!child) {
-              break;
-            }
-            child.image = getImae(child.image, this.SvgCollection);
+  export default {
+    name: "it-module",
+    computed: {
+      getModule() {
+        if (this.$store.getters.getAppData("ITMODULE")) {
+          if (!this.SvgCollection.length) {
+            return [];
           }
+          let list = this.$store.getters.getAppData("ITMODULE").data.itmenu
+            .moduleContent;
+
+          for (let object of list.paragraphItem) {
+            let inspaction = "";
+            object.children = object.children.filter(item => item.linkToRMD);
+            for (let child of object.children) {
+              if (!child) {
+                break;
+              }
+              child.image = getImage(child.image, this.SvgCollection);
+            }
+          }
+          return list;
         }
-        return list;
+        return {paragraphItem: []};
+      },
+      BaseUrl() {
+        return this.$store.getters.getAppData("ITMENU").data.itmenu.menu.baseUrl + "/?par3=;ITCALL";
+      },
+      SvgCollection() {
+        return this.$store.getters.getExistedIcons;
       }
-      return { paragraphItem: [] };
     },
-    BaseUrl() {
-      return (
-        this.$store.getters.getAppData("ITMENU").data.itmenu.menu.baseUrl +
-        "/?par3=;ITCALL,"
-      );
-    },
-    SvgCollection() {
-      return this.$store.getters.getExistedIcons;
-    }
-  },
-  methods: {
-    loadDataForComponents() {
-      if (
-        this.$route.params.module != "FAVORITE_MODULE" &&
-        this.$route.params.module
-      ) {
-        var datasource = {
-          query:
-            ' { itmenu { moduleContent(codeMenu:"' +
-            this.$route.params.module +
-            '"){  title tooltip  paragraphItem{ name: text  image codeMenu  isFolder children: nodes{ linkToRMD name: text image codeMenu isFolder } } } } } ',
-          schema: "ITPORTAL"
-        };
-        var key = "ITMODULE";
-        this.$store.dispatch("LoadDataForComponent", {
-          datasource,
-          key
-        });
-      } else {
-        this.$store.commit("setAppData", {
-          key: "ITMODULE",
-          data: {
+    methods: {
+      loadDataForComponents() {
+        if (
+          this.$route.params.module !== "FAVORITE_MODULE" && this.$route.params.module
+        ) {
+          let datasource = {
+            query:
+              ' { itmenu { moduleContent(codeMenu:"' +
+              this.$route.params.module +
+              '"){  title tooltip  paragraphItem{ name: text  image codeMenu  isFolder children: nodes{ linkToRMD name: text image codeMenu isFolder } } } } } ',
+            schema: "ITPORTAL"
+          };
+          let key = "ITMODULE";
+          this.$store.dispatch("LoadDataForComponent", {
+            datasource,
+            key
+          });
+        } else {
+          this.$store.commit("setAppData", {
+            key: "ITMODULE",
             data: {
-              itmenu: {
-                moduleContent: this.$store.getters.getAppData("ITMENU").data
-                  .itmenu.menu.moduleContent
+              data: {
+                itmenu: {
+                  moduleContent: this.$store.getters.getAppData("ITMENU").data
+                    .itmenu.menu.moduleContent
+                }
               }
             }
-          }
-        });
+          });
+        }
+      },
+      // Функция для обновления данных при изменении роутинга
+      beforeRouteUpdate(to, from, next) {
+        this.loadDataForComponents();
       }
     },
-    // Функция для обновления данных при изменении роутинга
-    beforeRouteUpdate(to, from, next) {
+    beforeMount() {
       this.loadDataForComponents();
     }
-  },
-  beforeMount() {
-    this.loadDataForComponents();
-  }
-};
+  };
 </script>
 
 <style scoped>
-.menu {
-  text-align: left;
-}
-.svg {
-  width: 20px;
-  height: 20px;
-}
+
+  .sub-menu {
+    height: 83vh;
+    padding: 0;
+    overflow: hidden;
+    overflow-y: auto;
+  }
+
+  .icon {
+    max-width: 25px;
+  }
+
+  .svg {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+    fill: #757575;
+  }
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+  }
+
 </style>
