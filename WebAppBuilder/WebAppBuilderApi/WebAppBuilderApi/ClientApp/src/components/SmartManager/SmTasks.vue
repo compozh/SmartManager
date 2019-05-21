1feb987d1726ab91aea7cca50de509f9070e44db
Igor Pevchev <pevchev@it-enterprise.com>
2019-05-21 19:49:26 +0300

    update(SmTasks): update component structure and styles

--- WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/components/SmartManager/SmTasks.vue
+++ WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/components/SmartManager/SmTasks.vue
@@ -1,34 +1,67 @@
-<template >
-  <div fill-height>
-    <h1>Задачи</h1>
-
-    <!-- {{tasks}}  -->
-    <v-flex>
-      <v-flex v-for="task in tasks" :key="task.id" pt-3>
-        <v-card pa-3>
-          <!-- <v-list-tile-action>
-                <v-icon>list</v-icon>
-          </v-list-tile-action>-->
-
-          <v-list-tile-content>
-            <v-list-tile-title>Идентификатор: {{task.id}}</v-list-tile-title>
-            <v-list-tile-title>Название задчи: {{task.name}}</v-list-tile-title>
-            <v-list-tile-title>Имя: {{task.addedFio}}</v-list-tile-title>
-            <v-list-tile-title>Дата добавления: {{task.dateadd}}</v-list-tile-title>
-          </v-list-tile-content>
+<template>
+  <v-container fluid pa-2>
+    <v-layout column>
+      <v-flex
+        xs12
+        mb-2
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        <v-card class="title task-item">
+          <v-layout row wrap align-center>
+            <v-flex xs3 sm2 md1>
+              <v-icon
+                size="50"
+                color="#b3b3b3"
+              >account_circle
+              </v-icon>
+            </v-flex>
+            <v-flex xs9 sm10 md11>
+              <v-layout column text-xs-left>
+                <v-flex>
+                  <span
+                    class="body-2 blue--text text--darken-2"
+                  >{{task.name}}</span>
+                </v-flex>
+                <v-flex>
+                  <span class="caption">Исполнитель: </span>
+                  <span class="body-1">{{task.addedFio}}</span>
+                </v-flex>
+                <v-flex>
+                  <v-card-actions class=" pl-0 caption grey--text">
+                    Дата добавления: {{task.dateadd}}
+                    <v-spacer></v-spacer>
+                    Идентификатор: {{task.id}}
+                  </v-card-actions>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
         </v-card>
       </v-flex>
-    </v-flex>
-  </div>
+    </v-layout>
+  </v-container>
 </template>
 
 
 <script>
-export default {
-  name: "sm-tasks",
-
-  props: ["tasks"]
-};
+  export default {
+    name: "sm-tasks",
+    props: ["tasks"]
+  };
 </script>
-<style>
+<style scoped>
+  .user-icon {
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    background-size: contain;
+    background-position: center;
+    margin-left: 25px;
+  }
+
+  .task-item:hover {
+    cursor: pointer;
+    background: rgb(250, 250, 250);
+  }
 </style>
