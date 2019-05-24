d0aff77313d090b280317b1fa56e8ea7e3b63791
Vitalii Bakhariev <baharev@it-enterprise.com>
2019-05-24 14:10:51 +0300

    Добавлены два компонента  PurchaseMenu, RouteMenu

---
+++ WebAppBuilder/WebAppBuilderApi/WebAppBuilderApi/ClientApp/src/components/common/PurchaseMenu.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-layout>
+        <user-panel><v-layout>
+        <purchase-route-links></purchase-route-links>
+    </v-layout>
+</template>
+
+<script>
+export default {
+name: 'purchase-menu'
+}
+</script>
+
+<style>
+
+</style>
