bf8aa5fe3a97c5e6647faca86225cdb5868f2810
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-03 19:35:40 +0300

    update(taskInfo): updated component functionality

--- WebAppBuilder/src/components/SmartManager/RenderLess/SmTaskInfo.js
+++ WebAppBuilder/src/components/SmartManager/RenderLess/SmTaskInfo.js
@@ -1,3 +1,5 @@
+import {eventBus} from "../../../main";
+
 export default {
   name: 'smTaskInfoRl',
   props: ['taskinfo'],
@@ -5,7 +7,10 @@ export default {
   data: () => ({
     menu: {
       tabs: [
-        'Задачи', 'Обсуждения', 'Связанные документы', 'История'
+        { name: 'Задачи', value: 'tasks', component: 'sm-task-tab-tasks' },
+        { name: 'Обсуждения', value: 'comments', component: 'sm-task-tab-comments' },
+        { name: 'Связанные документы', value: 'docs', component: 'sm-task-tab-docs' },
+        { name: 'История', value: 'history', component: 'sm-task-tab-history' }
       ],
       activeTab: null
     }
@@ -13,11 +18,34 @@ export default {
   computed: {
     taskDetail() {
       const smTasksInfo = this.$store.getters.getAppData("SMTASKINFO")
-      if (smTasksInfo) {
+      if (smTasksInfo && smTasksInfo.data) {
         return smTasksInfo.data.smtasks.tasksGetInfo;
       }
       return {}
     }
+  },
+  methods: {
+    loadDataForComponents() {
+      const datasource = {
+        query: `{ smtasks { tasksGetInfo(taskId:${this.$route.params.taskId}) { id arso name status addedId dateAdd isAgree addedFio comments { date text user } dateFact dateplan  descript keyValue priority originals { id comm date file ndor user fileName typeName typeDescription } addedPhoto dateRemind docPlandate }}} `,
+        schema: "SMARTMANAGER"
+      }
+      const key = "SMTASKINFO"
+      this.$store.dispatch("LoadDataForComponent", {
+        datasource,
+        key
+      });
+    },
+    // Функция для обновления данных при изменении роутинга
+    beforeRouteUpdate(to, from, next) {
+      this.loadDataForComponents();
+    },
+  },
+  beforeMount() {
+    this.loadDataForComponents();
+  },
+  updated() {
+    eventBus.$emit('setMenuMiniMode', true);
   },
   render() {
     return this.$scopedSlots.default({
