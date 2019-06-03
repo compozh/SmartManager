fb1ea3f57aec4bb9a372c40889423e35fb44da7a
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-03 19:40:00 +0300

    update(smtaskTabHistory): updated markup

--- WebAppBuilder/src/components/SmartManager/Views/SmTaskTabHistory.vue
+++ WebAppBuilder/src/components/SmartManager/Views/SmTaskTabHistory.vue
@@ -1,10 +1,18 @@
 <template>
-  $END$
+  <v-container pa-0>
+    <v-layout row wrap >
+      <v-flex xs12>
+        <p class="pt-3 grey--text subheading" >История отсутствует</p>
+      </v-flex>
+    </v-layout>
+    <v-divider></v-divider>
+  </v-container>
 </template>
 
 <script>
   export default {
-    name: "SmTaskTabHistory"
+    name: "smTaskTabHistory",
+    props: ['task']
   }
 </script>
 
