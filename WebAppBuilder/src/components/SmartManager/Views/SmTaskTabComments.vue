a861eac1baaabdd4571407a2c789c50b1740d172
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-03 19:39:43 +0300

    update(smtaskTabComments): updated markup

--- WebAppBuilder/src/components/SmartManager/Views/SmTaskTabComments.vue
+++ WebAppBuilder/src/components/SmartManager/Views/SmTaskTabComments.vue
@@ -1,13 +1,53 @@
 <template>
-  $END$
+  <v-container pa-0>
+    <v-layout
+      class="comment-item text-xs-left"
+      row wrap
+      v-for="(comment, index) in task.comments"
+      :key="index"
+    >
+      <v-flex
+        xs1
+        class="text-xs-center"
+      >
+        <v-icon
+          size="40"
+          color="#b3b3b3"
+        >account_circle
+        </v-icon>
+      </v-flex>
+      <v-flex px-2 class="text-xs-left">
+        <v-layout column text-xs-left>
+          <v-flex class="text-xs-left">
+            <span
+              class="body-2 font-weight-light blue--text text--darken-2"
+            >{{ comment.text }}</span>
+          </v-flex>
+          <v-flex
+            d-flex
+            justify-space-between
+            class="caption grey--text"
+          >
+            <span class="text-xs-left">{{ comment.user }}</span>
+            <span class="text-xs-right">{{ comment.date }}</span>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
-    name: "SmTaskComments"
+    name: "smTaskTabComments",
+    props: ['task']
   }
 </script>
 
 <style scoped>
+
+.comment-item {
+  border-bottom: 1px solid rgba(0,0,0,.12);
+}
 
 </style>
