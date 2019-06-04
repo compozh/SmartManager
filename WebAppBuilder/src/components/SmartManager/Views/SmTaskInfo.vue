b208a577d8bff9a6baeb551cae91f53701debbf0
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-04 19:56:15 +0300

    update(smTaskInfo): updated tabs style and markup, move documentViewer to docs tab

--- WebAppBuilder/src/components/SmartManager/Views/SmTaskInfo.vue
+++ WebAppBuilder/src/components/SmartManager/Views/SmTaskInfo.vue
@@ -2,32 +2,32 @@
   <v-container fluid mx-4 pa-0>
     <v-layout justify-center>
       <v-flex xs12>
-        <sm-task-info-rl v-slot="{ menu, task, props }">
+        <sm-task-info-rl
+          v-slot="{ activeTab, tabs, task, props }"
+        >
           <v-container fluid pa-0 ma-0>
             <v-layout>
               <v-flex xs12>
                 <v-layout>
-                  <v-flex xs6>
+                  <v-flex xs12>
                     <v-tabs
-                      centered
                       show-arrows
-                      fixed-tabs
-                      v-model="menu.activeTab"
+                      v-model="activeTab.value"
                     >
                       <v-tab
-                        v-for="(item, index) in menu.tabs"
-                        :key="index"
-                      >{{ item.name }}({{ item.count }})
+                        v-for="item in tabs"
+                        :key="item.value"
+                      >{{ item.name }}
                       </v-tab>
                     </v-tabs>
                     <v-divider></v-divider>
                     <v-tabs-items
-                      v-model="menu.activeTab"
+                      v-model="activeTab.value"
                     >
                       <v-tab-item
                         class="white tab-header"
-                        v-for="(item, index) in menu.tabs"
-                        :key="index"
+                        v-for="item in tabs"
+                        :key="item.value"
                       >
                         <component
                           :is="item.component"
@@ -35,45 +35,6 @@
                         ></component>
                       </v-tab-item>
                     </v-tabs-items>
-                  </v-flex>
-                  <v-flex xs6>
-                    <v-layout column>
-                      <v-flex>
-                        <p class="headline blue-grey--text">DOCUMENT VIEWER</p>
-                      </v-flex>
-                      <v-divider></v-divider>
-                      <v-flex xs6>
-                        <originals-viewer
-                          v-slot="{ originals, fileName, fileUrl, fileId, selectDocument }"
-                          :originals="task.originals"
-                        >
-                          <div>
-                            <v-layout column>
-                              <v-flex>
-                                <div
-                                  v-for="doc in originals"
-                                  :key="doc.id"
-                                >
-                                  <span
-                                    class="transfer"
-                                    :class="fileId === doc.id ? 'selected' : ''"
-                                    @click="selectDocument(doc.fileName, doc.file, doc.id)"
-                                  >
-                                    {{ doc.fileName }}
-                                  </span>
-                                </div>
-                              </v-flex>
-                              <v-flex px-2>
-                                <documentViewer
-                                  :fileName="fileName"
-                                  :fileUrl="fileUrl"
-                                ></documentViewer>
-                              </v-flex>
-                            </v-layout>
-                          </div>
-                        </originals-viewer>
-                      </v-flex>
-                    </v-layout>
                   </v-flex>
                 </v-layout>
               </v-flex>
