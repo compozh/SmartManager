23dc2597bf69852f32ba5872757e72d53e5cccba
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-07 18:16:45 +0300

    update(userPanel): added renderless component-

--- WebAppBuilder/src/components/common/UserPanel/UserPanelRl.js
+++ WebAppBuilder/src/components/common/UserPanel/UserPanelRl.js
@@ -1,19 +1,55 @@
 export default {
   name: 'userPanelRl',
   data: () => ({
-
+    menu: {
+      fav: true,
+      show: false,
+      message: false,
+      hints: true,
+    },
+    menuBtnStyle: [
+      { 'textTransform': 'none' },
+      { 'font-weight': 300 },
+      { color: 'rgb(102, 102, 102)' },
+      { 'border-color': '#c6c6c6' }
+    ]
   }),
   computed: {
-
+    getUserData() {
+      return this.$store.state.userData
+    },
+    getDelegatedRights() {
+      return this.$store.state.userData.DelegatedRights
+    }
   },
   methods: {
-
+    changePassword() {
+      console.log('Запрос изменения пароля')
+    },
+    logOut() {
+      this.$store.dispatch("LogOut");
+      this.$router.push({name: 'login'});
+    }
   },
   render() {
     return this.$scopedSlots.default({
-
+      menu: this.menu,
+      userData: this.getUserData,
       params: {
-        
+        changePassword: this.changePassword,
+        delegatRigthBtnAttr: {
+          items: ['user1', 'user2'],
+          style: this.menuBtnStyle
+        },
+        delegatRigthBtnAEvents: {
+
+        },
+        logOutBtnAttr: {
+          style: this.menuBtnStyle
+        },
+        logOutBtnAEvents: {
+          click: () => this.logOut()
+        }
       }
     })
   }
--- WebAppBuilder/src/components/common/UserPanel/Views/UserPanel.vue
+++ WebAppBuilder/src/components/common/UserPanel/Views/UserPanel.vue
@@ -1,36 +1,97 @@
 <template>
-    <v-toolbar flat class="transparent">
-        <v-list class="pa-0">
-          <v-list-tile avatar>
-            <v-list-tile-avatar>
-              <v-icon x-large>account_circle</v-icon>
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>{{currentUser}}</v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-action>
-              <v-btn icon ripple @click="onClick">
-                <v-icon color="grey">exit_to_app</v-icon>
-              </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
+  <user-panel-rl v-slot="{ menu, userData, params }">
+    <v-layout align-center>
+      <v-flex>
+        <user-icon :src="userData.Image" size="50"></user-icon>
+      </v-flex>
+      <v-flex class="hidden-and-up user-name">
+        <p class="ma-0 pl-2 subheading">{{ userData.Name }}</p>
+      </v-flex>
+      <v-flex>
+        <v-menu
+          v-model="menu.show"
+          :close-on-content-click="false"
+          :nudge-width="300"
+          :nudge-top="300"
+          :position-y="165"
+          bottom
+
+        >
+          <template v-slot:activator="{ on }">
+            <v-icon large v-on="on"
+            >
+              {{ menu.show ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}
+            </v-icon>
+          </template>
+          <v-layout column>
+            <v-flex>
+              <v-layout pa-2>
+                <v-flex class="grow-0">
+                  <user-icon
+                    class="user-big-image"
+                    :src="userData.Image"
+                    size="80"
+                  ></user-icon>
+                </v-flex>
+                <v-flex ml-3 class="text-xs-left">
+                  <p class="mb-1">{{ userData.Login }}</p>
+                  <a @click="params.changePassword"
+                  >Сменить пароль</a>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-divider></v-divider>
+            <v-flex>
+              <v-layout justify-space-between grey lighten-4 pa-2 class="#f5f5f5">
+                <v-flex class="grow-0">
+                  <v-btn
+                    outline small
+                    v-bind="params.delegatRigthBtnAttr"
+                    v-on="params.delegatRigthBtnAEvents"
+                  >Делегированные права</v-btn>
+                </v-flex>
+                <v-flex class="grow-0">
+                  <v-btn
+                    outline small
+                    v-bind="params.logOutBtnAttr"
+                    v-on="params.logOutBtnAEvents"
+                  >Выход</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-menu>
+      </v-flex>
+    </v-layout>
+  </user-panel-rl>
 </template>
+
 <script>
-export default {
-    name:'user-panel',
-    computed:{
-        currentUser(){
-            return this.$store.state.currentuser
-        }
-    },
-    methods:{
-        onClick(){
-            this.$store.dispatch("LogOut");
-            this.$router.push({name:'login'});
-        }
-    }
-}
+  export default {
+    name: 'user-panel'
+  }
 </script>
+
+<style scoped>
+
+  .v-menu__content {
+    background-color: white;
+    top: 65px !important;
+  }
+
+  .user-name {
+    white-space: nowrap;
+    width: 100%;
+  }
+
+
+
+  .grow-0 {
+    flex-grow: 0 !important;
+  }
+
+  a:hover {
+    text-decoration: underline #67A4E1;
+  }
+</style>
 
