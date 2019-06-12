e23250ca079b583e18428dedf2dab1fbf6ddb0df
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-12 16:23:42 +0300

    update(DelegatedRights): added "isActive" property

--- GraphQlServer/AuthenticationMiddleware/DelegatedRights.cs
+++ GraphQlServer/AuthenticationMiddleware/DelegatedRights.cs
@@ -7,6 +7,5 @@ namespace AuthenticationMiddleware
     {
         public string UserName { get; set; }
         public string UserId { get; set; }
-        public bool IsActive { get; set; }
     }
 }
