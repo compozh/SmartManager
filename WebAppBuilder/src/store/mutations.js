ffbb61a0f0b5e8308928e4aee78dec80c5f13494
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-12 16:31:39 +0300

    update(mutations): removed unused funtions

--- WebAppBuilder/src/store/mutations.js
+++ WebAppBuilder/src/store/mutations.js
@@ -2,46 +2,18 @@ import * as jsonpatch from '../patching'
 import Vue from 'vue'
 
 const mutations = {
-
   /** Описание приложения */
   SetAppDescription(state, application) {
-    state.applicationDescription = application;
+    state.applicationDescription = application
   },
-
   /** Загрузка хранилища */
   setAppData(state, payload) {
     Vue.set(state.appData, payload.key, payload.data)
   },
   /** Установка текущего пользователя */
   setCurrentUser(state, userInfo) {
-    state.currentUser = userInfo;
-  },
-  /** Установка данных пользователя */
-  setUserData(state, userInfo) {
-    state.userData = userInfo;
-  },
-  /** Установка массива с svg */
-  setExistedIcons(state, payload) {
-    state.existedIcons = payload;
+    state.currentUser = userInfo
   }
-  // /** Загрузка Layout приложения */
-  // setAppLayout(state, payload) {
-  //   Vue.set(state.appLayout, payload.key, payload.data)
-  // },
-  // /** Обновить данные патчем */
-  // updateData(state, {
-  //   key,
-  //   patchlist
-  // }) {
-  //   jsonpatch.applyPatchList(state.appData[key], patchlist);
-  // },
-  // /** Обновить Layout патчем */
-  // updateLayout(state, {
-  //   key,
-  //   patchlist
-  // }) {
-  //   jsonpatch.applyPatchList(state.appLayout[key], patchlist);
-  // }
-};
+}
 
 export default mutations
