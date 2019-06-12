c92a77137303d8dfccdff39895c4ff8a50d347ee
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-12 16:30:44 +0300

    update(getters): added new getters

--- WebAppBuilder/src/store/getters.js
+++ WebAppBuilder/src/store/getters.js
@@ -5,6 +5,21 @@ const getters = ({
   getCurrentUser(state) {
     return state.currentUser
   },
+  getCurrentUserLoginData(state) {
+    return state.currentUser
+          ? state.currentUser.LoginData
+          : {}
+  },
+  getCurrentUserRights(state) {
+    return state.currentUser
+          ? state.currentUser.DelegatedRights
+          : {}
+  },
+  getCurrentUserPhoto(state) {
+    return state.currentUser
+      ? state.currentUser.UserPhoto
+      : ""
+  },
   getExistedIcons(state) {
     return state.existedIcons;
   }
