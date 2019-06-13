07f18b086099a15beb332327200cf1cd7f5d2a2d
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-13 14:21:34 +0300

    create(SmartManager): create local store

---
+++ WebAppBuilder/src/components/SmartManager/api/store/state.js
@@ -0,0 +1,10 @@
+const state = {
+  applicationDescription:{},
+  appData: {},
+  // Текущий пользователь
+  currentUser: {},
+  // Массив с названием иконок
+  existedIcons:[]
+}
+
+export default state
