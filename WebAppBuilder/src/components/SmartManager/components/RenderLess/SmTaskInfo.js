c1dd56c62e4856eb880b42d4e04426932dde0f78
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-19 12:02:52 +0300

    update(components): set api for components

--- WebAppBuilder/src/components/SmartManager/components/RenderLess/SmTaskInfo.js
+++ WebAppBuilder/src/components/SmartManager/components/RenderLess/SmTaskInfo.js
@@ -1,51 +1,30 @@
-import {eventBus} from "../../../main";
-import gql from 'graphql-tag'
 export default {
   name: 'smTaskInfoRl',
   data: () => ({
-      tabs: [
-        {name: 'Задачи', value: 'tasks', component: 'sm-task-tab-tasks'},
-        {name: 'Документы', value: 'originals', component: 'sm-task-tab-docs'},
-        {name: 'Обсуждения', value: 'comments', component: 'sm-task-tab-comments'},
-        {name: 'Согласования', value: 'agreement', component: 'sm-task-tab-agree'}
-      ],
-      activeTab: {
-          value: null
-      },
-    infoAboutTask:null
+    tabs: [
+      {name: 'Задачи', value: 'tasks', component: 'sm-task-tab-tasks'},
+      {name: 'Документы', value: 'originals', component: 'sm-task-tab-docs'},
+      {name: 'Обсуждения', value: 'comments', component: 'sm-task-tab-comments'},
+      {name: 'Согласования', value: 'agreement', component: 'sm-task-tab-agree'}
+    ],
+    activeTab: {
+      value: null
+    }
   }),
-  apollo : {
-    infoAboutTask:{
-      query : gql`
-      query taskInfoQUery($taskId: Int!){
-        smtasks{
-          tasksGetInfo(taskId: $taskId){
-            id arso name status addedId dateAdd isAgree addedFio comments { date text user } dateFact dateplan  descript keyValue priority originals { id comm date file ndor user fileName typeName typeDescription } addedPhoto dateRemind docPlandate
-          }
-        }
-      }`,
-      // Параметр
-      variables() {
-        return{
-          taskId : this.$route.params.taskId
-        }
-      },
-       update(data){
-        return data
-      }
-    },
+  created() {
+    const taskId = +this.$route.params.taskId
+    const taskInfo = this.taskDetail
+    if (taskInfo === null || taskInfo.id !== taskId) {
+      this.$store.dispatch('getTaskInfo', taskId)
+    }
   },
   computed: {
     taskDetail() {
-      const smTasksInfo = this.infoAboutTask
-      if (smTasksInfo) {
-        return smTasksInfo.smtasks.tasksGetInfo;
-      }
-      return {}
+      return this.$store.getters.taskInfo
     },
     getNotEmptyTabs() {
       const taskObj = this.taskDetail
-      if (taskObj.id) {
+      if (taskObj) {
         return this.tabs.filter(i => {
           // вкладка с комментариями отображается всегда
           if (i.value === 'comments') {
@@ -65,12 +44,6 @@ export default {
       }
     }
   },
-  methods: {
-   
-  },
-  updated() {
-    //eventBus.$emit('setMenuMiniMode', true);
-  },
   render() {
     return this.$scopedSlots.default({
       activeTab: this.activeTab,
@@ -80,5 +53,3 @@ export default {
     })
   }
 }
-
-
