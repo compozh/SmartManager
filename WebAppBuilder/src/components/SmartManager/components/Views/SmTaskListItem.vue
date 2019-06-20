7adac112c2a49189d2c22a7d746bdabda6775fcf
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-20 16:01:41 +0300

    update(taskListItem): information icons group added to task view component

--- WebAppBuilder/src/components/SmartManager/components/Views/SmTaskListItem.vue
+++ WebAppBuilder/src/components/SmartManager/components/Views/SmTaskListItem.vue
@@ -1,22 +1,81 @@
 <template>
   <sm-tasks-item-rl
-    v-slot="{ taskDetailLink }"
+    v-slot="{ taskLink }"
   >
     <v-card
       flat
-      class="task-item"
-      @click="taskDetailLink(task.id)"
-    >priority: {{ task.priority }}
+      class="task-item pl-3"
+      :class="{ unread: !task.isRead }"
+      @click="taskLink(task.id)"
+    >
       <v-layout row align-center>
-        <v-flex d-flex justify-center shrink px-3>
+        <v-flex d-flex justify-center shrink pr-3>
           <user-icon :src="task.addedPhoto" size="50"></user-icon>
         </v-flex>
         <v-flex py-1 pr-2 class="text-ellipsis">
           <v-layout column text-xs-left>
-            <v-flex class="text-ellipsis">
-              <span
-                class="body-2 font-weight-light blue--text text--darken-2"
-              >{{ task.name }}</span>
+            <v-flex>
+              <v-layout>
+                <v-flex class="text-ellipsis">
+                  <span
+                    class="body-2 font-weight-light blue--text text--darken-2"
+                  >{{ task.name }}</span>
+                </v-flex>
+                <v-flex shrink>
+                  <span class="icon-group d-flex align-center">
+                    <v-tooltip top>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-if="task.priority" v-on="on">priority_high</v-icon>
+                      </template>
+                      <span>Высокий приоритет</span>
+                    </v-tooltip>
+                    <v-tooltip top>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-if="task.hasOrig" v-on="on">attach_file</v-icon>
+                      </template>
+                      <span>Есть вложения</span>
+                    </v-tooltip>
+                    <v-tooltip top>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-if="task.isAgree" class="ml-1" v-on="on">playlist_add_check</v-icon>
+                      </template>
+                      <span>Требует согласования</span>
+                    </v-tooltip>
+                    <v-tooltip top>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-if="task.hasComm" class="ml-1" v-on="on">chat_bubble_outline</v-icon>
+                      </template>
+                      <span>Есть коментарии</span>
+                    </v-tooltip>
+                    <v-tooltip top>
+                      <template v-slot:activator="{ on }">
+                        <v-icon v-if="task.isMy" class="ml-1" v-on="on">face</v-icon>
+                      </template>
+                      <span>Задача от меня</span>
+                    </v-tooltip>
+                    <v-tooltip top>
+                      <template v-slot:activator="{ on }">
+                        <v-icon
+                          v-on="on"
+                          :color="taskStatus.color"
+                          class="hidden-sm-and-up ml-1"
+                        >{{ taskStatus.icon }}</v-icon>
+                      </template>
+                      <span>{{ taskStatus.text }}</span>
+                    </v-tooltip>
+                    <v-chip
+                      disabled
+                      small outline
+                      class="hidden-xs-only ma-0 ml-1"
+                      :color="taskStatus.color"
+                    >
+                      <v-avatar>
+                        <v-icon size="18px">{{ taskStatus.icon }}</v-icon>
+                      </v-avatar>{{ taskStatus.text }}
+                    </v-chip>
+                  </span>
+                </v-flex>
+              </v-layout>
             </v-flex>
             <v-flex class="caption text-ellipsis">
               <span class="hidden-xs-only"
@@ -25,7 +84,7 @@
             <v-flex d-flex class="caption grey--text">
               <v-flex class="text-xs-left">
                 <span class="hidden-xs-only"
-                >Дата добавления: </span>{{ task.dateadd }}
+                >Добавлено: </span>{{ task.dateAdd }}
               </v-flex>
               <v-flex class="text-xs-right">
                 <span class="hidden-xs-only"
@@ -41,18 +100,46 @@
 
 <script>
   export default {
-    name: "SmTaskListItem",
-    props: ['task']
+    name: 'SmTaskListItem',
+    props: ['task'],
+    computed: {
+      taskStatus() {
+        switch (this.task.status) {
+          case '':
+            return {
+              color: 'orange darken-2',
+              icon: 'access_time',
+              text: 'ожидает'
+            }
+          case '*':
+            return {
+              color: 'blue-grey darken-1',
+              icon: 'loop',
+              text: 'в работе'
+            }
+          case '-':
+            return {
+              color: 'red darken-2',
+              icon: 'highlight_off',
+              text: 'отклонена'
+            }
+          case '+':
+            return {
+              color: 'green darken-2',
+              icon: 'check_circle_outline',
+              text: 'выполнена'
+            }
+        }
+      }
+    }
   }
 </script>
 
 <style scoped>
   .task-item {
     box-shadow: inset 0 -1px 0 rgba(100, 121, 143, 0.122);
-  }
-
-  .task-item:nth-of-type(odd) {
-    border-left: 5px solid #1976D2;
+    border-left: 5px solid transparent;
+    border-radius: 0;
   }
 
   .task-item:hover {
@@ -66,9 +153,20 @@
     text-overflow: ellipsis;
   }
 
+  .v-chip {
+    height: 22px !important;
+    font-size: 12px
+  }
 
+  .v-chip:focus {
+    background-color: red;
+  }
+
+  .v-chip >>> .v-chip__content {
+    cursor: pointer;
+  }
 
   .unread {
-    border-left: 5px solid #1976D2;
+    border-left-color: #1976D2;
   }
 </style>
