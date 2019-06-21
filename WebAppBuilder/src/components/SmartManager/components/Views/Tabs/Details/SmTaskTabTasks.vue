5ebdddeecce7232bedf9a77b0ed7a252d9b661cc
Igor Pevchev <pevchev@it-enterprise.com>
2019-06-21 15:00:22 +0300

    update(*): change file and directory structure

---
+++ WebAppBuilder/src/components/SmartManager/components/Views/Tabs/Details/SmTaskTabTasks.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid pa-0 px-2>
+    <v-layout row wrap justify-center>
+      <v-flex>
+        <sm-task-details-item :task="task"></sm-task-details-item>
+      </v-flex>
+      <v-flex
+        xs12
+        class="task-container"
+        v-for="subTask in taskDetail.tasks"
+        :key="subTask.id"
+      >
+        <sm-task-details-item :task="subTask"></sm-task-details-item>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "smTaskTabTasks",
+    props: ['taskDetail']
+  }
+</script>
+
+<style scoped>
+
+</style>
