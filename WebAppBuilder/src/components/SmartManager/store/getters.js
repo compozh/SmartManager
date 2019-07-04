9b1133c9158f3c5cec0244dc37da4955ae87a7da
Igor Pevchev <pevchev@it-enterprise.com>
2019-07-04 11:59:35 +0300

    update(search): added search functionality

--- WebAppBuilder/src/components/SmartManager/store/getters.js
+++ WebAppBuilder/src/components/SmartManager/store/getters.js
@@ -13,7 +13,39 @@ export default {
   },
   tasks(state) {
     const folderId = state.currentFolder
-    return state.tasks[folderId]
+    const tasks = state.tasks[folderId]
+    const search = state.search ? state.search.trim() : ''
+
+    if (!search || !tasks) {
+      return tasks
+    }
+    const searchFields = [
+      'id',
+      'addedFio',
+      'dateAdd',
+      'dateFact',
+      'dateplan',
+      'declarer',
+      'descript',
+      'htmlDescript',
+      'name',
+      'performer'
+    ]
+    return tasks.filter(task => {
+      for (let i = 0; i < searchFields.length; i++) {
+        let searchField = searchFields[i]
+        let taskField = task[searchField]
+
+        if (!taskField) {
+          continue
+        }
+        let taskFieldValue = String(taskField).toLowerCase()
+        let isMatching = taskFieldValue.includes(search.toLowerCase())
+        if (isMatching) {
+          return true
+        }
+      }
+    })
   },
   taskInfo(state) {
     return state.taskInfo
