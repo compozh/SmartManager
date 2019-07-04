78bb2a1b8f179048982543ef39182a2e04768eae
Igor Pevchev <pevchev@it-enterprise.com>
2019-07-04 13:10:28 +0300

    update(getters): added comments to search getter

--- WebAppBuilder/src/components/SmartManager/store/getters.js
+++ WebAppBuilder/src/components/SmartManager/store/getters.js
@@ -15,10 +15,11 @@ export default {
     const folderId = state.currentFolder
     const tasks = state.tasks[folderId]
     const search = state.search ? state.search.trim() : ''
-
+    // Определение необходимость поиска/фильтрации задач
     if (!search || !tasks) {
       return tasks
     }
+    // Список полей, по которым осуществляется поиск
     const searchFields = [
       'id',
       'addedFio',
@@ -32,15 +33,17 @@ export default {
       'performer'
     ]
     return tasks.filter(task => {
+      // Цикл for по ключам задачи для возможности использовать continue
       for (let i = 0; i < searchFields.length; i++) {
         let searchField = searchFields[i]
         let taskField = task[searchField]
-
+        // Если такого поля нет - пропуск без проверки
         if (!taskField) {
           continue
         }
         let taskFieldValue = String(taskField).toLowerCase()
         let isMatching = taskFieldValue.includes(search.toLowerCase())
+        // При первом же совпадении отбор и переход к следующей задаче
         if (isMatching) {
           return true
         }
