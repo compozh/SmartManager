b1697d508ca4d12342c2315c2821bc859fd03e7c
IT\Melentyev <melentyev@it.ua>
2019-07-08 18:09:00 +0300

    initial mes commit

---
+++ WebAppBuilder/src/components/MES/MesRoot.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>{{helloString}} <div @click="onLoadClick">LOAD!</div></div>
+</template>
+<script>
+export default {
+  name: "mes-root",
+
+  data(){
+    return {
+      helloString:""
+    }
+  },
+
+  created(){
+  //  setTimeout("")
+  },
+  mounted(){
+
+  },
+
+  methods:{
+    onLoadClick(){
+      this.helloString = "TEST"
+    }
+  }
+}
+</script>
+
