9f065cd44a3e114b589ab3e787dc408e977f4af6
Igor Pevchev <pevchev@it-enterprise.com>
2019-07-09 12:35:15 +0300

    update(smTasks): added brakepoint watcher for filter tabs

--- WebAppBuilder/src/components/SmartManager/components/RenderLess/SmTaskInfo.js
+++ WebAppBuilder/src/components/SmartManager/components/RenderLess/SmTaskInfo.js
@@ -24,9 +24,14 @@ export default {
     },
     getTabs() {
       if (this.task) {
-        return this.tabs
+        return this.hiddenLgAndUp
+          ? this.tabs.filter(tab => tab.value !== 'originals')
+          : this.tabs
       }
-    }
+    },
+    hiddenLgAndUp() {
+      return this.$vuetify.breakpoint.lgAndUp
+    },
   },
   render() {
     return this.$scopedSlots.default({
@@ -34,7 +39,8 @@ export default {
       tabs: this.getTabs,
       params: {
         hasOrig: this.task ? this.task.originals.length : 0,
-        hasComm: this.task ? this.task.comments.length : 0
+        hasComm: this.task ? this.task.comments.length : 0,
+        hiddenLgAndUp: this.hiddenLgAndUp
       }
     })
   }
