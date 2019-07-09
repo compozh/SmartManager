edcf8e83cf62f48c13bdeffe512031c5017759b0
Igor Pevchev <pevchev@it-enterprise.com>
2019-07-09 13:22:08 +0300

    update(pdfViewer): added buttons

--- WebAppBuilder/src/components/SmartManager/components/Views/Tabs/Attachments/SmPdfViewer.vue
+++ WebAppBuilder/src/components/SmartManager/components/Views/Tabs/Attachments/SmPdfViewer.vue
@@ -1,32 +1,106 @@
 <template>
   <v-layout column class="pdf-viewer">
     <v-flex class="viewer-toolbar">
-      <v-btn outline small class="viewer-btn" @click="back()">
+      <!-- Кнопка на страницу назад-->
+      <v-btn
+        outline small
+        class="viewer-btn"
+        @click="back"
+        :disabled="page === 1"
+      >
         <v-icon size="18">arrow_drop_up</v-icon>
       </v-btn>
-      <v-btn outline small class="viewer-btn" @click="forward()">
+      <!-- Кнопка на страницу вперед-->
+      <v-btn
+        outline small
+        class="viewer-btn"
+        @click="forward"
+        :disabled="page === numPages"
+      >
         <v-icon size="18">arrow_drop_down</v-icon>
       </v-btn>
-      <input type="number" min="1" :max="numPages" @input="input($event)" v-model.number="page">
+      <!-- Поле отображения/ввода текущей страницы -->
+      <input
+        type="number"
+        min="1"
+        :max="numPages"
+        @input="input($event)"
+        v-model.number="page"
+      >
       <span class="page-count">/</span>
-      <input type="number" min="1" v-model.number="numPages" disabled>
-      <v-btn outline small class="viewer-btn" @click="rotate += 90">
+      <!-- Поле отображения количества страниц -->
+      <input
+        type="number"
+        v-model="numPages"
+        disabled
+      >
+      <!-- Кнопка поворота на 90 градусов против часовой стрелки -->
+      <v-btn
+        outline small
+        class="viewer-btn"
+        @click="rotate += 90"
+      >
         <v-icon size="18">rotate_left</v-icon>
       </v-btn>
-      <v-btn outline small class="viewer-btn" @click="rotate -= 90">
+      <!-- Кнопка поворота на 90 градусов по часовой стрелке -->
+      <v-btn
+        outline small
+        class="viewer-btn"
+        @click="rotate -= 90"
+      >
         <v-icon size="18">rotate_right</v-icon>
       </v-btn>
-      <v-btn outline small class="viewer-btn" @click="setWidth(-10)">
+      <!-- Кнопка уменьшения масштаба на 10% -->
+      <v-btn
+        outline small
+        class="viewer-btn"
+        @click="setWidth(-10)"
+      >
         <v-icon size="18">zoom_out</v-icon>
       </v-btn>
-      <select class="zoom-options" v-model.number="width">
+      <!-- Поле отображения масштаба с возможностью выбора из выпадающего списка-->
+      <select
+        class="zoom-options"
+        v-model.number="width"
+      >
         <option
           v-for="(i, index) in (maxWidth - minWidth) / 10 + 1"
-        >{{ minWidth + index * 10 }}</option>
+        >
+          {{ minWidth + index * 10 }}
+        </option>
       </select>
       <span class="page-count">%</span>
-      <v-btn outline small class="viewer-btn" @click="setWidth(10)">
+      <!-- Кнопка увеличения масштаба на 10% -->
+      <v-btn
+        outline small
+        class="viewer-btn"
+        @click="setWidth(10)"
+      >
         <v-icon size="18">zoom_in</v-icon>
+      </v-btn>
+      <!-- Кнопка маштабирования по ширине страницы -->
+      <v-btn
+        outline small
+        class="viewer-btn"
+        @click="width = 100"
+      >
+        <v-icon size="18">panorama_horizontal</v-icon>
+      </v-btn>
+      <!-- Кнопка маштабирования по высоте страницы !!!НЕ РЕАЛИЗОВАНО -->
+      <v-btn
+        outline small
+        class="viewer-btn"
+        @click=""
+      >
+        <v-icon size="18">panorama_vertical</v-icon>
+      </v-btn>
+      <!-- Кнопка включения/выключения полноэкранного режима -->
+      <v-btn
+        outline small
+        class="viewer-btn"
+        @click="toggleFullScreen"
+      >
+        <v-icon size="18">open_with</v-icon>
       </v-btn>
     </v-flex>
     <v-flex class="viewer-wrapper">
@@ -46,6 +120,7 @@
             :page="i"
             :id="i"
             :rotate="rotate"
+            @page-loaded="observePages($event)"
           ></pdf>
         </v-flex>
       </v-layout>
@@ -67,9 +142,6 @@
       page: 1,
       numPages: 0,
       rotate: 0,
-      curPage: 0,
-      pageCount: 0,
-      sourceText: '',
       width: 100,
       minWidth: 60,
       maxWidth: 160
@@ -80,52 +152,31 @@
         .then(pdf => this.numPages = pdf.numPages)
         .catch(e => this.$store.commit('sm/setError', e.message))
     },
-    updated() {
-      // const isRotate = this.rotate / 90 % 2 ? 2 : 1
-      // const threshold = this.width <= 100 ? 0.6 : (0.6 / this.width * 100).toFixed(2)
-      // const options = {
-      //   root: null,
-      //   rootMargin: '0px',
-      //   threshold: threshold / isRotate
-      // }
-      // const callback = (entries, observer) => {
-      //   entries.forEach(entry => {
-      //     console.log('', entry.intersectionRatio)
-      //     if (entry.intersectionRatio > options.threshold
-      //       && this.page !== entry.target.id) {
-      //       this.page = entry.target.id
-      //     }
-      //   })
-      // }
-      // const observer = new IntersectionObserver(callback, options)
-      // for (let i = 1; i <= this.numPages; i++) {
-      //   let elem = document.getElementById(i)
-      //   observer.observe(elem);
-      // }
-      // window.onscroll = function () {
-      //   console.log('window.onscroll', )
-      // }
-      // for (let i = 1; i <= this.numPages; i++) {
-      //   let elem = document.getElementById('page' + i)
-      //   elem.addEventListener('onscroll', function () {
-      //     console.log('scroll', )
-      //   })
-      //   elem.addEventListener('scroll', function () {
-      //     console.log('scroll', )
-      //   })
-      //   elem.onscroll = function () {
-      //     console.log('elem.onscroll', )
-      //   }
-      //   elem.addEventListener('click', event => {
-      //
-      //     console.log('offsetTop - ', event.currentTarget.offsetTop)
-      //     console.log('scrollTop - ', event.currentTarget.scrollTop)
-      //     console.log('scrollLeft - ', event.currentTarget.scrollLeft)
-      //
-      //   })
-      // }
-    },
     methods: {
+      observePages(event) {
+        if (this.numPages === event) {
+          const callback = (entries, observer) => {
+            entries.forEach(entry => {
+              if (entry.isIntersecting && this.page !== entry.target.id) {
+                this.page = entry.target.id
+              }
+            })
+          }
+          const observer = new IntersectionObserver(callback, {threshold: 0.3})
+          for (let i = 1; i <= this.numPages; i++) {
+            let elem = document.getElementById(i)
+            observer.observe(elem);
+          }
+        }
+      },
+      toggleFullScreen() {
+        const elem = document.querySelector('.pdf-viewer')
+        if (!document.fullscreenElement) {
+          elem.requestFullscreen()
+        } else {
+          document.exitFullscreen()
+        }
+      },
       switchPage(id) {
         document.getElementById(id).scrollIntoView()
       },
@@ -167,17 +218,9 @@
     height: inherit;
   }
 
-  .viewer-wrapper {
-    overflow: auto;
-  }
-
   .viewer-toolbar {
     background: #efefef;
     border-bottom: 1px solid #c6c6c6;
-  }
-
-  .viewer-page-container {
-
   }
 
   .viewer-page-item {
@@ -217,8 +260,22 @@
     min-width: 30px;
   }
 
-  ::-webkit-scrollbar {
-    display: none;
+  .viewer-wrapper {
+    overflow: auto;
+  }
+
+  .viewer-wrapper::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+  }
+
+  .viewer-wrapper::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  }
+
+  .viewer-wrapper::-webkit-scrollbar-thumb {
+    background-color: darkgrey;
+    outline: 1px solid slategrey;
   }
 
 </style>
