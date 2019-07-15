2514402e3208590f5f31ff29a1631bcb429df18b
lapin.den1488 <lapin.den1488@gmail.com>
2019-07-15 17:11:23 +0300

    catalog view top 10

--- WebAppBuilder/src/components/Purchases/pages/Catalogue.vue
+++ WebAppBuilder/src/components/Purchases/pages/Catalogue.vue
@@ -27,22 +27,33 @@
           />
         </div>
         <div v-else>
-        <v-layout wrap justify-center :class="`${rowViewType ? 'row' : 'column'}`">
+        <v-layout wrap :class="`${rowViewType ? 'row' : 'column'}`">
           <template v-for="item in itemsComp">
             <v-flex :key="item.id" xs12 sm6 md4 lg3 catalogue-card>
+              <v-card>
               <router-link :to="{ name:'CATALOGUE', params: {catalogueId: item.id.trim() }}">
-                <v-card>
-                  <item-picture :entityName="entityType" :id="item.id" height="200px" width="200px"/>
-                  <span>
-                    {{item.name}}
-                  </span>
-                </v-card>
+                  <item-picture :entityName="entityType" :id="item.id" height="200px" width="365px"/>
+                  <div class="title mb-1">{{item.name}}</div>                  
               </router-link>
+              <v-list>
+              <template  v-for="(child,index) in item.children">              
+                <v-list-tile class="list_content" :key="child.id">                
+                  <v-list-tile-content>
+                    <v-list-tile-title v-text="child.name.slice(0,20)"></v-list-tile-title>
+                  </v-list-tile-content>
+                  <v-list-tile-action>
+                    <v-icon>keyboard_arrow_right</v-icon>
+                  </v-list-tile-action>
+                </v-list-tile>
+                  <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+              </template>
+              </v-list>
+              </v-card>
             </v-flex>
           </template>
         </v-layout>
         </div>
-    </div>
+    </div>          
     <div v-else>
       <v-card>
         <v-card-title>
@@ -71,6 +82,14 @@
         catalogueId: undefined
       },
       methods:{
+        getTop10(response, counter){
+          let resp_children = response.data.purchases.items.splice(0, 10);
+          this.$set(this.items[counter], "children", []);
+          for(let i=0;i<resp_children.length;i++){
+            this.items[counter].children.push(resp_children[i]);
+          }
+        },
+
         getItems(id){
           var fieldname = "name";
           if (!id){
@@ -87,29 +106,37 @@
           }
           api.getResourcesGroupsByGroup(id, `id,name: ${fieldname}`).then(this.respCallback);
         },
+
         respCallback (resp) {
             this.items = resp.data.purchases.items;
+            for(let i=0;i<this.items.length;i++){
+              api.getResourcesGroupsByGroup(this.items[i].id, `id,name: name`).then(r=> this.getTop10(r,i));
+            }
         },
-        searchCallback (item) { 
-          //debugger;
+        
+        searchCallback (item) {
           var itemToSearch = _.lowerCase(item.name);
           var searchedText = _.lowerCase(_.trim(this.search));
           var ret = itemToSearch.indexOf(searchedText);
           return ret >= 0;
         }
       },
+
       created: function () {
         var id = this.routeParamCode;
         this.items = [];
-        this.getItems(id);
+        this.getItems(id);        
       },
       computed:{
         itemsComp(){
           if (this.search === ""){
+            
             return this.items;
           }
+          
           return _.filter(this.items, this.searchCallback);
         },
+        
         routeParamCode(){
           return this.$route.params.catalogueId ? this.$route.params.catalogueId : "";
         },
@@ -122,15 +149,22 @@
         '$route' (to, from) {
           var id = to.params.catalogueId;
           this.getItems(id);
+          
         }
       }
   }
 </script>
 
 <style lang="scss" scoped>
+.list_content:hover{
+  background: lightgrey;
+}
+  .child{
+    float: left;
+  }
   .v-card {
     text-align: -webkit-center;
-    height: 250px;
+    min-height: 250px;
     margin: 5px !important;
     box-shadow: none;
   }
