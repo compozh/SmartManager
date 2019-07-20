95a9d898d8cde50f20890c117626a26b58fc3f9f
IT\Melentyev <melentyev@it.ua>
2019-07-20 12:59:18 +0300

    обновления модуля routerCore. - убрал зависимость от VueRouter

--- NpmPackages/routerCore/src/index.js
+++ NpmPackages/routerCore/src/index.js
@@ -1,18 +1,13 @@
 import Router from './Router'
-
-let _namespace = 'CoreRouter'
 export default {
-  install(Vue, { options, dependencies }) {
+  install(Vue, { dependencies }) {
     let { router } = dependencies
 
     if (!router) {
-      throw new Error('VueRouter не был обнаружен среди зависимостей по ключу "router"!')
+      throw new Error('экземпляр VueRouter не был обнаружен среди зависимостей по ключу "router"!')
 
     }
-
-    let coreRouter = new Router(router)
-
-    dependencies.modulesManager.register(_namespace, () => Promise.resolve(coreRouter))
+    let coreRouter = new Router(router, dependencies)
     Vue.prototype.$CoreRouter = coreRouter
   }
 }
