335aa4a36985d510bbdcd9bef6bfa904b99e6edd
IT\Melentyev <melentyev@it.ua>
2019-07-27 12:59:43 +0300

    authentication. добавлено делегирование, добавлено хранилище

--- NpmPackages/authentication/src/index.js
+++ NpmPackages/authentication/src/index.js
@@ -1,14 +1,58 @@
+import path from 'path'
 import Authentication from './authentication'
+import storeModule from './store/index'
+
+let Vue = undefined
 
 export default {
-  install(Vue, params) {
+  install(vue, params) {
     let { dependencies } = params || {}
+    Vue = vue
 
     if (!dependencies) {
-      throw new Error("Зависимости должны быть переданы!")
+      throw new Error('Зависимости должны быть переданы!')
     }
 
-    let authentication = new Authentication(dependencies)
+    // регистрируем модуль хранилища
+    let store = dependencies.store
+    if (!store) {
+      throw new Error('Хранилище должно быть передано в виде зависимости .store')
+    }
+    store.registerModule(storeModule.namespace, storeModule)
+
+    // регистрация компонентов
+    registerComponents(require.context('./components/renderless', false, /\.vue$|.js$/), 'rs')
+    registerComponents(require.context('./components/views', false, /\.vue$|.js$/), '')
+
+    loadLocalizations()
+
+    let authentication = new Authentication(Vue, dependencies)
     Vue.prototype.$authentication = authentication
   }
+}
+
+/**
+ * Загрузка локализаций
+ */
+function loadLocalizations() {
+
+  let context = require.context('./resources', false, /\.json$/)
+  context.keys().forEach(function (key) {
+    var name = path.basename(key)
+    name = name.substring(0,name.lastIndexOf('.'))
+    Vue.prototype.$localization.RegisterLanguage('eds', name, () => Promise.resolve(context(key)))
+  })
+
+}
+
+
+/**
+ *  Регистрация компонентов
+ */
+function registerComponents(context) {
+  context.keys().forEach(function (key) {
+    var name = path.basename(key)
+    name = name.substring(0,name.lastIndexOf('.'))
+    Vue.component(`${name}`, context(key).default)
+  })
 }
