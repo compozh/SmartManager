81aa87125ecd18722ef6cf1a03e33f7c8d38f948
a_shev4enko <olexander.shev4enko@gmail.com>
2019-07-29 15:13:50 +0300

    MES. VueJs. Доработка темплейтов

--- WebAppBuilder/src/components/MES/MesContainer.vue
+++ WebAppBuilder/src/components/MES/MesContainer.vue
@@ -1,10 +1,12 @@
 <template>
   <v-container>
-    <v-layout column wrap >
+    <v-layout column wrap>
       <v-flex class="button-toolbar">
         <mes-toolbar/>
       </v-flex>
-      <v-flex>
+      <v-flex style="max-height: 400px"
+      class="scroll-y"
+      >
         <mes-element/>
       </v-flex>
     </v-layout>
@@ -15,6 +17,13 @@ import {mapGetters} from 'vuex'
 
 export default {
   name: "mes-container",
+
+  methods: {
+
+      // onScroll (e) {
+      //   this.offsetTop = e.target.scrollTop
+      // }
+  },
 }
 </script>
 <style type="text/css" scoped>
--- WebAppBuilder/src/components/MES/MesElements.vue
+++ WebAppBuilder/src/components/MES/MesElements.vue
@@ -1,34 +1,67 @@
 <template>
-<v-component>
-  <v-layout>
-  <v-flex>
-    <v-card-text>Parapapapfsa</v-card-text>
-  </v-flex>
-  <v-flex>
-    <v-card-text>Parapapapfsa</v-card-text>
-  </v-flex>
-  </v-layout>
-<v-component>
+    <v-layout>
+    <v-flex class="list-block">
+      <v-card class="card"
+      v-for="task in tasks"
+      :key="task.id"
+      @click="onCardClick(task.id)"
+      >
+        <v-card-text>Task name: {{task.name}}</v-card-text>
+        <v-card-text>Start time: {{task.data.startTime}}</v-card-text>
+        <v-card-text>End time: {{task.data.endTime}}</v-card-text>
+        <v-card-text>Status: {{task.data.status}}</v-card-text>
+      </v-card>
+
+
+      <v-card-text>Parapapapfsa</v-card-text>
+    </v-flex>
+    <v-flex>
+      <v-card-text>Parapapapfsa</v-card-text>
+    </v-flex>
+    </v-layout>
 </template>
 <script>
 import {mapGetters} from 'vuex'
 
 export default {
-  name: "mes-element",
-  created(){
-    this.getProperties()
+  data () {
+    var items = [
+      {name: 'tasks-1', id:'task-1', data:{ startTime:'12 May 2019', endTime: '30 July 2019', status: 'closed' }},
+      {name: 'tasks-2', id:'task-2', data:{ startTime:'12 May 2019', endTime: '28 September 2019', status: 'In Progress' }},
+      {name: 'tasks-3', id:'task-3', data:{ startTime:'12 May 2019', endTime: '12 July 2019', status: 'closed' }},
+      {name: 'tasks-4', id:'task-4', data:{ startTime:'12 May 2019', endTime: '15 September 2019', status: 'In Progress' }},
+      {name: 'tasks-5', id:'task-5', data:{ startTime:'12 May 2019', endTime: '12 July 2019', status: 'closed' }},
+      {name: 'tasks-6', id:'task-6', data:{ startTime:'12 May 2019', endTime: '23 July 2019', status: 'closed' }},
+      {name: 'tasks-7', id:'task-7', data:{ startTime:'12 May 2019', endTime: '06 September 2019', status: 'In Progress' }},
+      {name: 'tasks-8', id:'task-8', data:{ startTime:'12 May 2019', endTime: '05 July 2019', status: 'closed' }},
+      {name: 'tasks-9', id:'task-9', data:{ startTime:'12 May 2019', endTime: '11 September 2019', status: 'In Progress' }},
+    ]
+    return {tasks: items}
   },
+  name: "mes-element",
+
   computed:{
-    ...mapGetters({
-        loading: 'mes/properties',
-      }),
+
   },
     methods: {
+      onCardClick(data){
+        console.log(data)
+      },
 
-    getProperties() {
-      const loader = this.properties ? 'setLinearLoader' : 'setCircularLoader'
-      this.$store.dispatch('mes/getProperties')
-    }
   }
 }
 </script>
+<style type="text/css" scoped>
+  .list-block {
+    width: 500px;
+    max-width: 500px;
+
+  }
+  .list-block .card {
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    margin: 20px;
+    border-radius: 20px;
+  }
+</style>
