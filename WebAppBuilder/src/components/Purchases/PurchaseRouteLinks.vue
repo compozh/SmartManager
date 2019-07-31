0ddf750c12d2f29c80e173e176ed0ffa999ab998
Дмитрий Максимюк <maximuk.dmitriy@live.ru>
2019-07-31 17:28:51 +0300

    delete debugger calls

--- WebAppBuilder/src/components/Purchases/PurchaseRouteLinks.vue
+++ WebAppBuilder/src/components/Purchases/PurchaseRouteLinks.vue
@@ -24,7 +24,6 @@ export default {
     name:"purchase-route-links",
     computed:{
         links(){
-          //debugger
             if(!this.$store.state.applicationDescription){
                 return []
             }
