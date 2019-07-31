c3fb3a5a27b02c9fe1caeb925f59fd7dbb52380f
a_shev4enko <olexander.shev4enko@gmail.com>
2019-07-31 14:28:38 +0300

    Mes. VueJs. Доработка тулбара для тасков.

--- WebAppBuilder/src/components/MES/components/MesTasksToolbar.vue
+++ WebAppBuilder/src/components/MES/components/MesTasksToolbar.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-layout row>
-    <v-flex class="setup-material-button">
-      <v-btn v-if="layout !== 'mes-task-setup-materials-layout'" outlined @click="onclickSetupMaterial($event)">Установить материалы</v-btn>
-   </v-flex>
-   <v-flex class="accept-button">
-      <v-btn v-if="layout !== 'mes-task-setup-materials-layout'" outlined @click="onclickAccept($event)">Взять в работу</v-btn>
-   </v-flex>
-   <v-flex class="downtime-button">
-      <v-btn v-if="layout !== 'mes-task-setup-materials-layout'" outlined @click="onclickDowntime($event)">Простой</v-btn>
-   </v-flex>
-   <v-flex class="downtime-button">
-      <v-btn v-if="layout === 'mes-task-setup-materials-layout'" outlined @click="onclickDowntime($event)">Снять все партии</v-btn>
-   </v-flex>
-   <v-flex xs12 md4>
-      <v-text-field v-if="layout === 'mes-task-setup-materials-layout'"
+  <v-layout row xs12>
+    <v-flex
+      class="toolbar-basebuttons"
+      grow
+      v-if="layout !== 'mes-task-setup-materials-layout'">
+      <v-btn
+      outlined @click="onclickSetupMaterial($event)">Установить материалы</v-btn>
+      <v-btn
+      outlined :color="this.applyWorkColor" @click="onclickAccept($event)">Взять в работу</v-btn>
+    </v-flex>
+    <v-flex
+    grow
+    v-if="layout === 'mes-task-setup-materials-layout'">
+      <v-text-field
+        class="qr-input"
         v-model="firstname"
         :rules="nameRules"
         :counter="10"
@@ -21,6 +21,18 @@
         required
       ></v-text-field>
     </v-flex>
+   <v-flex
+   xs2
+   v-if="layout !== 'mes-task-setup-materials-layout'">
+      <v-btn
+      outlined color="error" @click="onclickDowntime($event)">Простой</v-btn>
+   </v-flex>
+   <v-flex
+   xs2
+   v-if="layout === 'mes-task-setup-materials-layout'">
+     <v-btn
+     outlined @click="onclickDowntime($event)">Снять все партии</v-btn>
+   </v-flex>
   </v-layout>
 </template>
 
@@ -29,6 +41,9 @@ import {mapGetters} from 'vuex'
 
 export default {
   name: "mes-tasks-toolbar",
+  data(){
+    return {applyWorkColor: 'success'}
+  },
  props: {
     layout: String
   },
@@ -39,10 +54,13 @@ export default {
     onclickAccept: function(event, state) {
         let target = event.target;
         debugger;
-        if(target.state === "accept") {
+        if(!target.state || target.state === "accept") {
+          debugger;
+          this.applyWorkColor = 'error';
           target.textContent = "Остановить";
           target.state = "suspend";
         } else {
+          this.applyWorkColor = 'success';
           target.state = "accept";
            target.textContent = "Взять в работу";
         }
@@ -50,18 +68,18 @@ export default {
     onclickDowntime: function(event) {
 
     }
-  } 
+  }
 }
 </script>
 
 <style type="text/css" scoped>
-.setup-material-button {
-  left: 0;  
-}
-.accept-button {
-
-}
-.downtime-button {
-  right: 0;  
-}
+  .toolbar-basebuttons{
+    display: flex;
+    justify-content: flex-start;
+    padding-left: 25px;
+  }
+  .qr-input{
+    padding-left: 25px;
+    width: 500px;
+  }
 </style>
