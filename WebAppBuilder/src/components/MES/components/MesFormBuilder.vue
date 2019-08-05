39c86d786328ae33455450780a04e269e43ccdf5
a_shev4enko <olexander.shev4enko@gmail.com>
2019-08-05 13:04:56 +0300

    Mes VueJs Доработка стилей генератора форм

--- WebAppBuilder/src/components/MES/components/MesFormBuilder.vue
+++ WebAppBuilder/src/components/MES/components/MesFormBuilder.vue
@@ -1792,5 +1792,32 @@ export default {
 .formio-container /deep/ {
   @import "~bootstrap/dist/css/bootstrap.min";
   @import "~choices.js/public/assets/styles/choices.css";
+  font-size: 14px;
+  font-weight: 500;
+  label {
+    margin-top: .5rem !important;
+    font-size: 16px;
+  }
+  .row.formio-component.formio-component-columns{
+    padding: 0 5px;
+  }
+  .form-group.has-feedback.formio-component-submit {
+    margin-top: 1rem;
+  }
+  .editgrid-add {
+    margin: 1rem;
+  }
+  .btn.btn-default.btn-sm.editRow {
+    width: 50px;
+    background-color: #007bff;
+    color: white;
+  }
+  .table.datagrid-table.table-bordered thead {
+    font-size: 16px;
+  }
+  .list-group-item.list-group-header.formio-edit-grid-header {
+    font-size: 16px;
+  }
 }
+
 </style>
