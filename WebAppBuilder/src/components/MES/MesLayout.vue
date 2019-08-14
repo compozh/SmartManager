a299f702d1b42fae88667a122d4da93657b69742
a_shev4enko <olexander.shev4enko@gmail.com>
2019-08-14 09:41:27 +0300

    Mes VueJs Доработка navMenu

--- WebAppBuilder/src/components/MES/MesLayout.vue
+++ WebAppBuilder/src/components/MES/MesLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="mes-app">
-    <v-navigation-drawer v-if="currentUser" app clipped hide-overlay :mini-variant="menuMiniMode && this.$vuetify.breakpoint.mdAndDown" v-model="drawer">
+    <v-navigation-drawer v-if="currentUser" app clipped hide-overlay :mini-variant="menuMiniMode" v-model="drawer">
       <router-view name="navigation-drawer"/>
     </v-navigation-drawer>
     <v-toolbar app fixed clipped-left extended :extension-height="3">
@@ -79,7 +79,7 @@ export default {
   },
   methods: {
       toggleMenuMode() {
-        if (this.$vuetify.breakpoint.mdAndUp) {
+        if (this.$vuetify.breakpoint.lg || this.$vuetify.breakpoint.xl) {
           this.$store.dispatch("mes/toggleMenuMiniMode")
         }
         else {
