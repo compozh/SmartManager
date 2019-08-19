37807d1c53ddb0808507087b4071c821c2a40ca0
a_shev4enko <olexander.shev4enko@gmail.com>
2019-08-19 11:24:53 +0300

    Mes VueJs Доработка контент-лодера

--- WebAppBuilder/src/components/MES/components/ContentLoader.vue
+++ WebAppBuilder/src/components/MES/components/ContentLoader.vue
@@ -1,26 +1,49 @@
 <template>
     <div class="mes-content-loader">
-        <ContentLoader v-if="this.loaderType == 'list'">
-            <rect x="0" y="0" rx="3" ry="3" width="400" height="10" />
-            <rect x="20" y="20" rx="3" ry="3" width="220" height="10" />
-            <rect x="20" y="40" rx="3" ry="3" width="170" height="10" />
-            <rect x="0" y="60" rx="3" ry="3" width="400" height="10" />
-            <rect x="20" y="80" rx="3" ry="3" width="200" height="10" />
-            <rect x="20" y="100" rx="3" ry="3" width="350" height="10" />
+        <ContentLoader v-if="this.loaderType == 'list'" height=700>
+            <rect x="0" y="0" rx="3" ry="3" width="400" height="130" />
+            <rect x="0" y="140" rx="3" ry="3" width="400" height="130" />
+            <rect x="0" y="280" rx="3" ry="3" width="400" height="130" />
+            <rect x="0" y="420" rx="3" ry="3" width="400" height="130" />
+            <rect x="0" y="560" rx="3" ry="3" width="400" height="130" />
         </ContentLoader>
-        <ContentLoader v-if="this.loaderType != 'list'" :height="this.loaderHeight">
-            <rect x="0" y="0" rx="3" ry="3" width="75" height="75" />
-            <rect x="80" y="0" rx="3" ry="3" width="75" height="75" />
-            <rect x="160" y="0" rx="3" ry="3" width="75" height="75" />
-            <rect x="240" y="0" rx="3" ry="3" width="75" height="75" />
-            <rect x="320" y="0" rx="3" ry="3" width="75" height="75" />
-            <rect x="400" y="0" rx="3" ry="3" width="75" height="75" />
-            <rect x="0" y="80" rx="3" ry="3" width="75" height="75" />
-            <rect x="80" y="80" rx="3" ry="3" width="75" height="75" />
-            <rect x="160" y="80" rx="3" ry="3" width="75" height="75" />
-            <rect x="240" y="80" rx="3" ry="3" width="75" height="75" />
-            <rect x="320" y="80" rx="3" ry="3" width="75" height="75" />
-            <rect x="400" y="80" rx="3" ry="3" width="75" height="75" />
+        <ContentLoader v-if="this.loaderType != 'list'" height=300>
+            <rect x="0" y="0" rx="3" ry="3" width="75" height="40" />
+            <rect x="80" y="0" rx="3" ry="3" width="75" height="40" />
+            <rect x="160" y="0" rx="3" ry="3" width="75" height="40" />
+            <rect x="240" y="0" rx="3" ry="3" width="75" height="40" />
+            <rect x="320" y="0" rx="3" ry="3" width="75" height="40" />
+            <rect x="400" y="0" rx="3" ry="3" width="75" height="40" />
+            <rect x="0" y="50" rx="3" ry="3" width="75" height="40" />
+            <rect x="80" y="50" rx="3" ry="3" width="75" height="40" />
+            <rect x="160" y="50" rx="3" ry="3" width="75" height="40" />
+            <rect x="240" y="50" rx="3" ry="3" width="75" height="40" />
+            <rect x="320" y="50" rx="3" ry="3" width="75" height="40" />
+            <rect x="400" y="50" rx="3" ry="3" width="75" height="40" />
+            <rect x="0" y="100" rx="3" ry="3" width="75" height="40" />
+            <rect x="80" y="100" rx="3" ry="3" width="75" height="40" />
+            <rect x="160" y="100" rx="3" ry="3" width="75" height="40" />
+            <rect x="240" y="100" rx="3" ry="3" width="75" height="40" />
+            <rect x="320" y="100" rx="3" ry="3" width="75" height="40" />
+            <rect x="400" y="100" rx="3" ry="3" width="75" height="40" />
+            <rect x="0" y="150" rx="3" ry="3" width="75" height="40" />
+            <rect x="80" y="150" rx="3" ry="3" width="75" height="40" />
+            <rect x="160" y="150" rx="3" ry="3" width="75" height="40" />
+            <rect x="240" y="150" rx="3" ry="3" width="75" height="40" />
+            <rect x="320" y="150" rx="3" ry="3" width="75" height="40" />
+            <rect x="400" y="150" rx="3" ry="3" width="75" height="40" />
+            <rect x="0" y="200" rx="3" ry="3" width="75" height="40" />
+            <rect x="80" y="200" rx="3" ry="3" width="75" height="40" />
+            <rect x="160" y="200" rx="3" ry="3" width="75" height="40" />
+            <rect x="240" y="200" rx="3" ry="3" width="75" height="40" />
+            <rect x="320" y="200" rx="3" ry="3" width="75" height="40" />
+            <rect x="400" y="200" rx="3" ry="3" width="75" height="40" />
+            <rect x="0" y="250" rx="3" ry="3" width="75" height="40" />
+            <rect x="80" y="250" rx="3" ry="3" width="75" height="40" />
+            <rect x="160" y="250" rx="3" ry="3" width="75" height="40" />
+            <rect x="240" y="250" rx="3" ry="3" width="75" height="40" />
+            <rect x="320" y="250" rx="3" ry="3" width="75" height="40" />
+            <rect x="400" y="250" rx="3" ry="3" width="75" height="40" />
         </ContentLoader>
     </div>
 </template>
@@ -31,7 +54,7 @@ import {ContentLoader} from 'vue-content-loader'
 
 export default {
   data(){
-    return { loaderHeight: 180 }
+    return { loaderHeight: 200 }
   },
   name: "mes-content-loader",
   components: {
@@ -46,5 +69,6 @@ export default {
 <style type="text/css" scoped>
     .mes-content-loader {
         padding: 20px;
+        overflow: hidden;
     }
 </style>
