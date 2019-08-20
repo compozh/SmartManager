ee602de3858ffd2c8dad495bc595a1a604763cdd
IT\Melentyev <melentyev@it.ua>
2019-08-20 15:58:36 +0300

    шаблон приложения. доработка авторизации, конфигурирование шаблона

---
+++ Applications/AppTemplate/src/templateConfig.js
@@ -0,0 +1,8 @@
+export default {
+  login: {
+    // Возможные варианты: ['google', 'facebook', 'twitter', 'github']
+    allowedAlternativeLoginMethods: ['google', 'facebook', 'twitter', 'github'],
+    allowedRegisterUser: false
+  }
+
+}
