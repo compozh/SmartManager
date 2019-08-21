28ce3062fd661110f73e94bfd1b33f2e4ce3dcaf
vodopyanov_vlad <vodopyanov@it.ua>
2019-08-21 11:18:34 +0300

    mes.vuejs

--- Applications/Mes/src/components/MesLayout.vue
+++ Applications/Mes/src/components/MesLayout.vue
@@ -64,7 +64,7 @@ export default {
   },
   computed: {
     currentUser() {
-      return this.$store.getters.getCurrentUser;
+      return this.$store.state.authentication.currentUser;
     },
     snackbar() {
       return this.$store.getters["mes/snackbar"];
