e2ee5302996c4be43069d4b63d67e9bd981610a7
Igor Pevchev <pevchev@it-enterprise.com>
2019-09-10 16:23:43 +0300

    update(smVuesax): status chips added

--- Applications/SmartManagerVuesax/src/views/task-list/TaskListItem.vue
+++ Applications/SmartManagerVuesax/src/views/task-list/TaskListItem.vue
@@ -13,14 +13,18 @@
       ></vs-avatar>
 
       <div class="flex w-full justify-between items-start">
-        <div class="task__details truncate">
+        <div class="task__details truncate mr-3">
           <h5 class="mb-1" :class="{'font-semibold': !task.isRead}"
           >{{ task.performer }}
           </h5>
           <span>{{ task.descript }}</span>
         </div>
+        <div class="task__details truncate mr-3">
+          <h5 :style="{color: task.isGenerate ? 'blue' : 'black' }">IsGenerate: {{ task.isGenerate }}</h5>
+          <h5>ARSO: {{ task.arso }}</h5>
+        </div>
 
-        <div class="task-item__meta flex items-center">
+        <div class="task-item__meta flex items-center flex-shrink-0">
           <span>{{ task.dateAdd }}</span>
         </div>
       </div>
@@ -28,9 +32,16 @@
 
     <!-- TASK ROW 2 : MSG & ACTIONS -->
     <div class="flex w-full">
-      <div class="task__message truncate ml-3">
+      <div class="task__message truncate mx-3">
         <span>{{ task.name | filter_tags }}</span>
       </div>
+      <vs-spacer></vs-spacer>
+      <vs-chip
+        :color="taskStatus().color"
+        class="flex-shrink-0"
+        style="flex-basis: 80px"
+      >{{ taskStatus().text }}
+      </vs-chip>
     </div>
   </div>
 </template>
@@ -46,6 +57,42 @@ export default {
   computed: {
     comments() {
       return this.task.comments ? this.task.comments.length : 0
+    },
+    taskStatus() {
+      return () => {
+        switch (this.task.status) {
+        case '':
+          return {
+            color: 'warning',
+            icon: 'loop',
+            text: this.$t('statuses.inWork')
+          }
+        case '*':
+          return {
+            color: 'primary',
+            icon: 'loop',
+            text: this.$t('statuses.inWork')
+          }
+        case '-':
+          return {
+            color: 'danger',
+            icon: 'highlight_off',
+            text: this.$t('statuses.rejected')
+          }
+        case '+':
+          return {
+            color: 'success',
+            icon: 'check_circle_outline',
+            text: this.$t('statuses.completed')
+          }
+        default:
+          return {
+            color: '',
+            icon: '',
+            text: ''
+          }
+        }
+      }
     }
   },
   methods: {
