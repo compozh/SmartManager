cb0ff835d9d900d2fd6d339ca8338d7aad6a92da
a_shev4enko <olexander.shev4enko@gmail.com>
2019-09-10 14:03:40 +0300

    Mes VueJs Доработка темплейта выработки

--- Applications/Mes/src/components/pages/Productions.vue
+++ Applications/Mes/src/components/pages/Productions.vue
@@ -1,12 +1,12 @@
 <template>
     <v-layout class="mes-productions">
-
-      <mes-content-loader v-if="!initializeProductions && !productions.length" />
       <mes-production-toolbar/>
-      <mes-productions-component :productions=productions />
+      <div class="production-main-block">
+        <mes-content-loader v-if="!initializeProductions && !productions.length"/>
+        <mes-productions-component :productions=productions />
 
-      <span class="no-data-text" v-if="initializeProductions && productions.length == 0">Нет факта регистрации выработки за смену</span>
-
+        <span class="no-data-text" v-if="initializeProductions && productions.length == 0">Нет факта регистрации выработки за смену</span>
+      </div>
     </v-layout>
 </template>
 
@@ -79,37 +79,46 @@ export default {
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
-    padding: 0 10px;
-    position: absolute;
     height: 100%;
+    overflow-x: hidden;
+    width: 100%;
+  }
+  .production-main-block {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    padding: 0 10px;
+    margin-top: 50px;
+    position: absolute;
+    height: calc(100%-50px);
     overflow-y: auto;
     overflow-x: hidden;
     width: 100%;
   }
-.mes-productions::-webkit-scrollbar {
+.production-main-block::-webkit-scrollbar {
     background-color:#fff;
     width:16px
   }
-  .mes-productions::-webkit-scrollbar-track {
+  .production-main-block::-webkit-scrollbar-track {
       background-color:#fff
   }
-  .mes-productions::-webkit-scrollbar-track:hover {
+  .production-main-block::-webkit-scrollbar-track:hover {
       background-color:#f4f4f4
   }
 
   /* scrollbar itself */
-  .mes-productions::-webkit-scrollbar-thumb {
+  .production-main-block::-webkit-scrollbar-thumb {
       background-color:#babac0;
       border-radius:16px;
       border:5px solid #fff
   }
-  .mes-productions::-webkit-scrollbar-thumb:hover {
+  .production-main-block::-webkit-scrollbar-thumb:hover {
       background-color:#a0a0a5;
       border:4px solid #f4f4f4
   }
 
   /* set button(top and bottom of the scrollbar) */
-  .mes-productions::-webkit-scrollbar-button {display:none}
+  .production-main-block::-webkit-scrollbar-button {display:none}
   .mes-content-loader {
     position: absolute;
     z-index: 1;
