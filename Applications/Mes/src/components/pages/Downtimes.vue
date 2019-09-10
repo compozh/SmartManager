d9223c7d44f140ba97597cc60322cf5997a015be
vodopyanov_vlad <vodopyanov@it.ua>
2019-09-10 18:57:18 +0300

    mes.vuejs

--- Applications/Mes/src/components/pages/Downtimes.vue
+++ Applications/Mes/src/components/pages/Downtimes.vue
@@ -67,6 +67,9 @@ export default {
       }
     },
     changeCurrentDowntime(newSelectedDowntime) {
+      if(this.selectedDowntime == newSelectedDowntime) {
+        return
+      }
       this.selectedDowntime = newSelectedDowntime
       this.$store.dispatch('mes/initializeDowntimeFormio', this.workCenter)
     },
