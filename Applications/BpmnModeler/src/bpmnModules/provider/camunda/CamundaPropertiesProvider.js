e9a9d65bb1da3c1f84b598b1db0fab42edfbf7d9
Дмитрий Петренко <petrenko@it.ua>
2019-10-03 12:11:31 +0300

    BpmnModeler. Добавлена настройка приоритета внешней задачи

--- Applications/BpmnModeler/src/bpmnModules/provider/camunda/CamundaPropertiesProvider.js
+++ Applications/BpmnModeler/src/bpmnModules/provider/camunda/CamundaPropertiesProvider.js
@@ -1,5 +1,8 @@
 import inherits from 'inherits';
 import PropertiesActivator from 'bpmn-js-properties-panel/lib/PropertiesActivator';
+import ImplementationTypeHelper from 'bpmn-js-properties-panel/lib/helper/ImplementationTypeHelper';
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
+import { is } from 'bpmn-js/lib/util/ModelUtil';
 
 // Require all properties you need from existing providers.
 // In this case all available bpmn relevant properties without camunda extensions.
@@ -11,8 +14,24 @@ import idProps from 'bpmn-js-properties-panel/lib/provider/bpmn/parts/IdProps';
 import nameProps from 'bpmn-js-properties-panel/lib/provider/bpmn/parts/NameProps';
 import executableProps from 'bpmn-js-properties-panel/lib/provider/bpmn/parts/ExecutableProps';
 import serviceTaskDelegateProps from 'bpmn-js-properties-panel/lib/provider/camunda/parts/ServiceTaskDelegateProps';
+import externalTaskConfiguration from 'bpmn-js-properties-panel/lib/provider/camunda/parts/ExternalTaskConfigurationProps';
 
 import formProps from './parts/FormProps';
+
+var isExternalTaskPriorityEnabled = function (element) {
+  var businessObject = getBusinessObject(element);
+
+  // show only if element is a process, a participant ...
+  if (is(element, 'bpmn:Process') || is(element, 'bpmn:Participant') && businessObject.get('processRef')) {
+    return true;
+  }
+
+  var externalBo = ImplementationTypeHelper.getServiceTaskLikeBusinessObject(element),
+    isExternalTask = ImplementationTypeHelper.getImplementationType(externalBo) === 'external';
+
+  // ... or an external task with selected external implementation type
+  return !!ImplementationTypeHelper.isExternalCapable(externalBo) && isExternalTask;
+};
 
 function createGeneralTabGroups(
   element, canvas, bpmnFactory,
@@ -37,6 +56,14 @@ function createGeneralTabGroups(
   linkProps(detailsGroup, element, translate);
   eventProps(detailsGroup, element, bpmnFactory, elementRegistry, translate);
 
+  var externalTaskGroup = {
+    id: 'externalTaskConfiguration',
+    label: translate('External Task Configuration'),
+    entries: [],
+    enabled: isExternalTaskPriorityEnabled
+  };
+  externalTaskConfiguration(externalTaskGroup, element, bpmnFactory, translate);
+
   var documentationGroup = {
     id: 'documentation',
     label: translate('Documentation'),
@@ -48,6 +75,7 @@ function createGeneralTabGroups(
   return [
     generalGroup,
     detailsGroup,
+    externalTaskGroup,
     documentationGroup
   ];
 
