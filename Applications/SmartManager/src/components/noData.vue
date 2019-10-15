86eef510752e59cba64d5671570b06f5e74144cd
Igor Pevchev <pevchev@it-enterprise.com>
2019-10-15 17:21:59 +0300

    create(smVuesax): created new component for empty state

---
+++ Applications/SmartManager/src/components/noData.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="no-data-container">
+    <slot>No data</slot>
+  </div>
+</template>
+
+<style scoped>
+  .no-data-container {
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    font-size: 2.25rem;
+    color: #dae1e7;
+    text-align: center;
+  }
+</style>
