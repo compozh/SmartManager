db7f016bca5fc99c3a925ddd023876256c3daf06
Igor Pevchev <pevchev@it-enterprise.com>
2019-10-22 20:21:51 +0300

    create(viewer): image and text viewers added

--- Applications/SmartManager/src/views/task-view/task-attachments/TaskAttachments.vue
+++ Applications/SmartManager/src/views/task-view/task-attachments/TaskAttachments.vue
@@ -1,61 +1,87 @@
 <template>
-  <VuePerfectScrollbar
-    class="scroll-area md:px-8 p-4"
-    :settings="settings"
-  >
-    <div class="vx-row h-full">
-      <div class="vx-col w-full h-full">
-        <vx-card class="px-4 h-full">
-
-          <pdf-viewer :url="url"></pdf-viewer>
-
-        </vx-card>
+  <div class="flex flex-col h-full bg-white" ref="container">
+    <div class="vx-row mx-3 p-3" ref="items">
+      <div>
+      </div>
+      <div class="flex flex-wrap">
+        <div
+          class="mail__attachment my-1"
+          v-for="(attachment, index) in attachments"
+          :key="index"
+          @click="currentFile = attachment"
+        >
+          <vx-tooltip
+            :text="attachment.fileName"
+            color="rgb(98, 98, 98, .95)"
+          >
+            <vs-chip
+              :color="attachment.id === currentFile.id ? 'success' : 'primary'"
+              class="mr-3 max-w-sm cursor-pointer"
+            >
+              <span class="flex mr-2">
+                <file-icon :extention="attachment.fileExt"></file-icon>
+              </span>
+              <span class="custom-truncate">{{ attachment.fileName }}</span>
+            </vs-chip>
+          </vx-tooltip>
+        </div>
       </div>
     </div>
-  </VuePerfectScrollbar>
+    <component :is="viewer" :url="url"></component>
+  </div>
 </template>
 
 <script>
-import VuePerfectScrollbar from 'vue-perfect-scrollbar'
-import { FileIcon } from 'vue-feather-icons'
 import PdfViewer from '@/components/pdf-viewer/Viewer'
+import ImgViewer from '@/components/ImageViewer'
+import TxtViewer from '@/components/TextViewer'
+import NotSupport from '@/components/NotSupport'
+import FileIcon from '@/components/FileIcon'
 
 export default {
   props: {
     attachments: Array,
-    url: String
+    index: Number
   },
   components: {
-    VuePerfectScrollbar,
-    FileIcon,
-    PdfViewer
+    PdfViewer,
+    ImgViewer,
+    TxtViewer,
+    NotSupport,
+    FileIcon
   },
   data: () => ({
-    //url: 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf',
-    currentFile: {},
-    settings: {
-      maxScrollbarLength: 60,
-      wheelSpeed: 0.50,
-    },
+    currentFile: {}
   }),
   computed: {
-    // viewAttachment() {
-    //   if (Object.keys(this.currentFile).length) {
-    //     return this.currentFile
-    //   }
-    //   return this.currentFile = this.attachments[this.index]
-    // }
+    url() {
+      if (!Object.keys(this.currentFile).length) {
+        this.currentFile = this.attachments[this.index]
+      }
+      return this.currentFile.fileUrl
+    },
+    isImage() {
+      const image = ['png', 'jpeg', 'jpg', 'bmp']
+      return ext => image.some(i => i === ext)
+    },
+    isText() {
+      const text = ['txt', 'log']
+      return ext => text.some(i => i === ext)
+    },
+    viewer() {
+      const ext = this.url.split('.').pop()
+      switch (true) {
+        case ext === 'pdf': return 'pdf-viewer'
+        case this.isText(ext): return 'txt-viewer'
+        case this.isImage(ext): return 'img-viewer'
+        default: return 'not-support'
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
-
-  .vx-card >>> .vx-card__collapsible-content.vs-con-loading__container,
-  .vx-card >>> .vx-card__collapsible-content .vx-card__body {
-    height: 100%;
-    padding: 0;
-  }
 
   .custom-truncate {
     display: block;
@@ -65,6 +91,5 @@ export default {
     text-overflow: ellipsis !important;
     white-space: nowrap !important;
   }
-
 
 </style>
