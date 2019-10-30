f070483522de9ebd87c12174c39249e29e454c77
IT\Melentyev <melentyev@it.ua>
2019-10-30 13:34:51 +0200

    small fix

--- Applications/Portal/public/config.js
+++ Applications/Portal/public/config.js
@@ -1,6 +1,5 @@
 window.appConfig = {
-  GrapgQlUrl: window.devMode ? 'http://localhost:15504/' : '/GraphQlServer/',
-  // GrapgQlUrl: window.devMode ? 'http://localhost/' : '/GraphQlServer/',
+  GrapgQlUrl: window.devMode ? 'http://localhost:5002/' : '/GraphQlServer/',
   BASE_URL: window.VUE_APP_BASE_PATH,
   WsUrl: window.devMode ? 'http://localhost:64137/ws/api/' : '/ws/api/',
   SignalRUrl: 'https://m.it.ua/ws/signalr/hubs'
