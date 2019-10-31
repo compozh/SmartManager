39a82c4e12bbf18c4803bbe3b50edbd65e1b6aed
Igor Pevchev <pevchev@it-enterprise.com>
2019-10-31 17:44:32 +0200

    update(packages): uninstall unused packages

--- Applications/SmartManager/vue.config.js
+++ Applications/SmartManager/vue.config.js
@@ -1,14 +1,6 @@
 const path = require('path')
 module.exports = {
   chainWebpack: config => {
-    //const svgRule = config.module.rule('svg')
-
-    //svgRule.uses.clear()
-
-    // svgRule
-    //   .use('vue-svg-loader')
-    //   .loader('vue-svg-loader')
-
     config.module
       .rule('graphql')
       .test(/\.(graphql|gql)$/)
