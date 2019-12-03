33ea62f46e40f64bed651691ab13624dd1206051
Igor Pevchev <pevchev@it-enterprise.com>
2019-12-03 17:57:27 +0200

    sm: change case status functionality added

--- Applications/SmartManager/src/store/app/smActions.js
+++ Applications/SmartManager/src/store/app/smActions.js
@@ -126,16 +126,17 @@ export default {
       throw Error
     }
   },
-  async changeStatus({dispatch}, payload) {
-    const status = JSON.stringify(payload)
+  async changeStatus({dispatch}, params) {
+    const paramsJson = JSON.stringify(params)
     startLoading(true)
     try {
-      const response = await api.changeTaskStatusInGql(status)
+      const response = await api.changeTaskStatusInGql(paramsJson)
       stopLoading()
       const result = response.data.smtasksMutation.changeStatus
       if (result.success) {
-        await dispatch('getTaskInfo', {
-          taskId: payload.id,
+        await dispatch('updateInfo', {
+          type: params.type,
+          id: params.id,
           loading: true
         })
         notify('success', 'statusTitle', 'statChangeSuccess')
