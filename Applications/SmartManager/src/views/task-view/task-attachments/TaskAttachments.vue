603627b6ecf78d7e52af7134616f160197b2b51e
Igor Pevchev <pevchev@it-enterprise.com>
2019-12-03 17:53:43 +0200

    sm: additional image and text file extensions added to viewer

--- Applications/SmartManager/src/views/task-view/task-attachments/TaskAttachments.vue
+++ Applications/SmartManager/src/views/task-view/task-attachments/TaskAttachments.vue
@@ -71,11 +71,11 @@ export default {
         : null
     },
     isImage() {
-      const image = ['png', 'jpeg', 'jpg', 'bmp']
+      const image = ['png', 'jpeg', 'jpg', 'webp', 'bmp', 'gif']
       return ext => image.some(i => i === ext)
     },
     isText() {
-      const text = ['txt', 'log']
+      const text = ['txt', 'log', 'ini', 'dll', 'bat', 'config', 'json']
       return ext => text.some(i => i === ext)
     },
     viewer() {
