4d57425898e9b904652d72107c549d87e2fd3f2f
Дмитрий Петренко <petrenko@it.ua>
2020-01-08 13:51:45 +0200

    WorkflowModeler. Удалены ненужные пакеты

--- Applications/BpmnModeler/vue.config.js
+++ Applications/BpmnModeler/vue.config.js
@@ -27,7 +27,9 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? process.env.VUE_APP_BASE_PATH + path.basename(process.cwd(), path.extname(process.cwd()))
     : '/',
-  configureWebpack: {
-    devtool: 'eval-source-map'
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: true
+    }
   }
 }
