cc8e5e6b1573a1c5a2973a0a283eec47131745be
Igor Pevchev <pevchev@it-enterprise.com>
2020-02-05 18:15:19 +0200

    sm: new components added

---
+++ Applications/SmartManager/src/views/task-view/task-attachments/AttachmentRemarks.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Attachment remarks</h1>
+</template>
+
+<script>
+export default {
+  name: 'attachment-remarks'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Applications/SmartManager/src/views/task-view/task-attachments/AttachmentVersions.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Attachment versions</h1>
+</template>
+
+<script>
+export default {
+  name: 'attachment-versions'
+}
+</script>
+
+<style scoped>
+
+</style>
