59bdbd09152602169ee484ffa5728643180d681b
Дмитрий Петренко <petrenko@it.ua>
2020-02-24 18:07:16 +0200

    ForceBPM Modeler. Доработка настройки действий

--- Applications/BpmnModeler/src/api/models/ActionDefinition.js
+++ Applications/BpmnModeler/src/api/models/ActionDefinition.js
@@ -1,8 +1,8 @@
 import ActionDefinitionType from './ActionDefinitionType';
 
 export default class ActionDefinition {
-  constructor({ id, name = '', type = ActionDefinitionType.ExternalTask, unformio, isSystem = false } = {}) {
-    this.id = id;
+  constructor({ code, name = '', type = ActionDefinitionType.ExternalTask, unformio, isSystem = false } = {}) {
+    this.id = code;
     this.name = name;
     this.type = type;
     this.unformio = unformio;
