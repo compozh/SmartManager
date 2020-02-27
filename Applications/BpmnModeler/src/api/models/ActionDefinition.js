1801d85963bb0a4f74d673521dffa1d9dff5d0b7
Александр Шевченко <olexander.shev4enko@gmail.com>
2020-02-27 12:00:28 +0200

    BPMN Доработка запросов на получение событий

--- Applications/BpmnModeler/src/api/models/ActionDefinition.js
+++ Applications/BpmnModeler/src/api/models/ActionDefinition.js
@@ -1,8 +1,8 @@
 import ActionDefinitionType from './ActionDefinitionType';
 
 export default class ActionDefinition {
-  constructor({ code, name = '', type = ActionDefinitionType.ExternalTask, unformio, isSystem = false } = {}) {
-    this.id = code;
+  constructor({ id, name = '', type = ActionDefinitionType.ExternalTask, unformio, isSystem = false } = {}) {
+    this.id = id;
     this.name = name;
     this.type = type;
     this.unformio = unformio;
