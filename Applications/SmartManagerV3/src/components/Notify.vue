461f1284bee3a6abdd1e03894728f91877f23ef3
Igor Pevchev <pevchev@it-enterprise.com>
2020-03-23 20:58:11 +0200

    sm_v3: notify component created

---
+++ Applications/SmartManagerV3/src/components/Notify.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-snackbar v-model="notify"
+              bottom right
+              :color="color"
+              multi-line
+              :timeout=5000>
+    {{ text }}
+    <v-btn dark text @click="notify = null">
+      {{ $t('buttons.close') }}
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'Notify',
+  computed: {
+    notify: {
+      get () {
+        return this.$store.state.app.notify
+      },
+      set () {
+        this.$store.commit('SET_NOTIFY', null)
+      }
+    },
+    color () {
+      return this.notify && this.notify.color
+        ? this.notify.color
+        : 'secondary'
+    },
+    text () {
+      return this.notify && this.notify.text
+        ? this.notify.text
+        : this.$t('messages.noMessages')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
