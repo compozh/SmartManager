76576a3abb19cec7ac1f2aa6e2a965430098f28f
Igor Pevchev <pevchev@it-enterprise.com>
2020-03-23 20:44:26 +0200

    sm_v3: app store updated

--- Applications/SmartManagerV3/src/store/app/mutations.js
+++ Applications/SmartManagerV3/src/store/app/mutations.js
@@ -1,8 +1,23 @@
 export default {
+  SET_APPLICATIONS_PARAMS (state, params) {
+    state.applicationParams = JSON.parse(params)
+  },
+  SET_PRELOADER (state, preLoader) {
+    state.preLoader = preLoader
+  },
   TOGGLE_SIDEBAR_OPEN (state) {
     state.sideBarOpen = !state.sideBarOpen
   },
   SET_SEARCH (state, search) {
     state.search = search
+  },
+  SET_NOTIFY (state, notify) {
+    state.notify = notify
+  },
+  SET_FOLDERS (state, folders) {
+    state.folders = folders.sort(i => i.code === 'active' ? -1 : 0)
+  },
+  SET_CURRENT_FOLDER (state, folder) {
+    state.currentFolder = folder
   }
 }
