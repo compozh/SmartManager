4f7185bcf8af37c8e49ad156929f224d62d04db7
Igor Pevchev <pevchev@it-enterprise.com>
2020-03-23 20:41:42 +0200

    sm_v3: app stor main file renamed to index.js

---
+++ Applications/SmartManagerV3/src/components/PreLoader.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-overlay app :value="preLoader">
+    <v-progress-circular
+      :size="50"
+      color="primary"
+      indeterminate
+    >
+      <v-img src="@/assets/logo.png"/>
+    </v-progress-circular>
+  </v-overlay>
+</template>
+
+<script>
+export default {
+  name: 'ThePreLoader',
+  computed: {
+    preLoader () {
+      return this.$store.state.app.preLoader
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
