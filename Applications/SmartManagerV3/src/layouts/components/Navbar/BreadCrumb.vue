ed7484ba7d9d3aa8fa6e9b66fb4713b152ec3161
Igor Pevchev <pevchev@it-enterprise.com>
2020-03-23 20:53:08 +0200

    sm_v3: breadCrumb component created

---
+++ Applications/SmartManagerV3/src/layouts/components/Navbar/BreadCrumb.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-breadcrumbs :items="items" class="hidden-md-and-down">
+    <template v-slot:divider>
+      <fa-icon :icon="['fal', 'chevron-right']" size="xs"/>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+<script>
+export default {
+  name: 'BreadCrumb',
+  data: () => ({
+    items: [
+      {
+        text: 'Login',
+        disabled: false,
+        exact: true,
+        to: '/login'
+      },
+      {
+        text: 'Link 1',
+        disabled: false,
+        href: 'breadcrumbs_link_1'
+      },
+      {
+        text: 'Link 2',
+        disabled: true,
+        href: 'breadcrumbs_link_2'
+      }
+    ]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
