f5876f07df823ef186fa59030911518eb76c7635
Igor Pevchev <pevchev@it-enterprise.com>
2020-03-23 20:54:03 +0200

    sm_v3: routing added to app

--- Applications/SmartManagerV3/src/router/routes.js
+++ Applications/SmartManagerV3/src/router/routes.js
@@ -3,7 +3,31 @@ export default [
     // =============================================================================
     // MAIN LAYOUT ROUTES
     // =============================================================================
+    path: '/',
+    component: () => import('@/layouts/Main.vue'),
+    children: [
+      {
+        path: '',
+        redirect: 'tasks/active'
+      },
+      {
+        path: 'tasks/:folderId',
+        name: 'task-list',
+        component: () => import('@/views/tasks/task-list/TaskList.vue')
+      }
+    ]
+  },
+  {
+    // =============================================================================
+    // FULL PAGE LAYOUT ROUTES
+    // =============================================================================
     path: '',
-    component: () => import('@/layouts/Main.vue')
+    component: () => import('@/layouts/FullPage.vue'),
+    children: [
+      {
+        path: 'login',
+        component: () => import('@/pages/Login.vue')
+      }
+    ]
   }
 ]
