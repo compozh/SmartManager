0303e4baf8c3c15e1e320dfb866f8f7ea78fa148
Igor Pevchev <pevchev@it-enterprise.com>
2020-03-27 08:54:05 +0200

    sm_v3: tasks store updated

--- Applications/SmartManagerV3/src/store/tasks/mutations.js
+++ Applications/SmartManagerV3/src/store/tasks/mutations.js
@@ -1,5 +1,8 @@
 export default {
   SET_TASKS (state, tasks) {
     state.tasks = Object.assign({}, state.tasks, tasks)
+  },
+  SET_TASK_DETAILS (state, task) {
+    state.taskDetails = Object.assign({}, state.taskDetails, task)
   }
 }
