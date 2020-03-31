ebcdf4c135f3bc3f1fdd0d00774e4904467c69df
Igor Pevchev <pevchev@it-enterprise.com>
2020-03-31 22:15:21 +0300

    sm_v3: components markup updated

--- Applications/SmartManagerV3/src/views/tasks/task-list/TaskList.vue
+++ Applications/SmartManagerV3/src/views/tasks/task-list/TaskList.vue
@@ -1,40 +1,41 @@
 <template>
-  <v-container fluid>
+  <v-row no-gutters>
+    <v-col>
+      <v-row dense align-content="start" justify="start">
+        <v-col class="d-flex">
+          <h4 class="ml-1 mt-1">{{ activeZone.title }}:
+            <span class="primary--text">{{ activeFolder.Name }}</span>
+            <span class="grey--text"> ({{ activeFolder.Count }})</span>
+          </h4>
+          <v-spacer/>
+          <v-btn-toggle v-model="viewMode" mandatory>
+            <v-btn small>
+              <fa-icon :icon="['fal', 'list-alt']" size="lg"/>
+            </v-btn>
+            <v-btn small>
+              <fa-icon :icon="['fal', 'table']" size="lg"/>
+            </v-btn>
+          </v-btn-toggle>
+        </v-col>
+      </v-row>
 
-    <v-row dense align-content="start" justify="start">
-      <v-col class="d-flex">
-        <h4 class="ml-1 mt-1">{{ activeZone.title }}:
-          <span class="primary--text">{{ activeFolder.Name }}</span>
-          <span class="grey--text"> ({{ activeFolder.Count }})</span>
-        </h4>
-        <v-spacer/>
-        <v-btn-toggle v-model="viewMode" mandatory>
-          <v-btn small>
-            <fa-icon :icon="['fal', 'list-alt']" size="lg"/>
-          </v-btn>
-          <v-btn small>
-            <fa-icon :icon="['fal', 'table']" size="lg"/>
-          </v-btn>
-        </v-btn-toggle>
-      </v-col>
-    </v-row>
+      <v-row v-if="viewMode">
+        <v-col>
+          <v-data-table
+            :headers="headers"
+            :items="tasks"
+            :items-per-page="10"
+            class="elevation-1 caption"
+            style="font-size: 10px"
+          ></v-data-table>
+        </v-col>
+      </v-row>
 
-    <v-row v-if="viewMode">
-      <v-col>
-        <v-data-table
-          :headers="headers"
-          :items="tasks"
-          :items-per-page="10"
-          class="elevation-1 caption"
-          style="font-size: 10px"
-        ></v-data-table>
-      </v-col>
-    </v-row>
+      <data-iterator v-else :tasks="tasks"/>
 
-    <data-iterator v-else :tasks="tasks"/>
-
-    <action-button/>
-  </v-container>
+      <action-button/>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
