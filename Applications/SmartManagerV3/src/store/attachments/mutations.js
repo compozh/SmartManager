f43d542959d7cd92246be34f86d5974a36bb0e8d
Igor Pevchev <pevchev@it-enterprise.com>
2020-04-06 09:54:12 +0300

    sm_v3: attachment store created

---
+++ Applications/SmartManagerV3/src/store/attachments/mutations.js
@@ -0,0 +1,26 @@
+export default {
+  SET_ATTACHMENT_TYPES (state, types) {
+    state.attachmentTypes = types
+  },
+  SET_ATTACHMENT_DETAILS (state, { fileId, fileDetails, taskOrCaseId: id, rootState }) {
+    if (fileDetails.ErrorMessage) {
+      state.currentAttachment = {
+        fileId,
+        access: false,
+        reason: fileDetails.ErrorMessage
+      }
+      return
+    }
+    const task = rootState.tasks.taskDetails[id] || rootState.cases.caseDetails[id]
+    const attachment = task.originals.find(original => {
+      return original.id === fileId
+    })
+    if (attachment) {
+      attachment.access = true
+      attachment.url = fileDetails.FileUrl
+      attachment.srcUrl = fileDetails.SrcUrl + attachment.fileName
+      attachment.versions = fileDetails.Versions
+      state.currentAttachment = Object.assign({}, attachment)
+    }
+  }
+}
