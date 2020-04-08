967756c45939b5f658aca4ac0ac3f25effc13e4b
Александр Шевченко <olexander.shev4enko@gmail.com>
2020-04-08 12:21:10 +0300

    SM Доработка карточек задач v.2

--- Applications/SmartManagerV3/src/views/tasks/task-list/TaskListItem.vue
+++ Applications/SmartManagerV3/src/views/tasks/task-list/TaskListItem.vue
@@ -14,7 +14,6 @@
         <fa-icon v-if="task.isGenerated" class="ml-2 " :icon="['fal', 'layer-plus']"/>
         <fa-icon v-if="task.isMy" class="ml-2 orange--text" :icon="['fal', 'portrait']"/>
         <fa-icon v-if="task.myControl" class="ml-2 indigo--text" :icon="['fal', 'eye']"/>
-        <!-- <fa-icon v-if="task.priority" class="ml-2 red--text" :icon="['fal', 'exclamation-triangle']"/> -->
       </div>
     </div>
     <v-divider class="mx-2"></v-divider>
@@ -27,25 +26,33 @@
         <div class="column-element">
           <span class="body-2 text--secondary">{{ task.addedFio }}</span>
           <span class="ma-0 pl-0 subtitle-1"><fa-icon v-if="task.priority == 1" class="red--text" :icon="['fal', 'fire']"/> {{ task.name }} </span>
-          <div>
-          <span v-if="task.role == 'COEXECUTOR'" class="caption">{{$t('tasks.coexecutor')}} </span>
-            <span v-if="task.role == 'CONTROLER'" class="red--text caption">{{$t('tasks.controler')}} </span>
-            <span v-if="task.childCount > 0 && task.childCount != task.childDoneCount" class="caption text--secondary"><fa-icon class="text--secondary" :icon="['fal', 'hourglass']"/> {{$t('tasks.childsAssigned')}}</span>
-            <span v-if="task.childCount > 0 && task.childCount == task.childDoneCount" class="green--text caption"><fa-icon class="green--text" :icon="['fal', 'check-double']"/> {{$t('tasks.childsDone')}} </span>
+          <div class="item-status">
+            <div class="declarer-role">
+              <span v-if="task.role == 'COEXECUTOR'" class="caption">{{$t('tasks.coexecutor')}} </span>
+              <span v-if="task.role == 'CONTROLER'" class="red--text caption">{{$t('tasks.controler')}} </span>
+              <span v-if="task.role == 'EXECUTOR'" class="blue--text caption">{{$t('tasks.executor')}} </span>
+              <span v-if="task.role == 'INITIATOR'" class="red--text caption">{{$t('tasks.initiator')}} </span>
+              <span v-if="task.role == 'INFORM'" class=" text--secondary caption">{{$t('tasks.inform')}} </span>
+            </div>
+            <div class="task-status">
+              <span v-if="task.childCount > 0 && task.childCount != task.childDoneCount && task.role != 'INITIATOR'" class="caption text--secondary"><fa-icon class="text--secondary" :icon="['fal', 'hourglass']"/> {{$t('tasks.childsAssigned')}}</span>
+              <span v-if="task.childCount > 0 && task.childCount == task.childDoneCount && task.role != 'INITIATOR'" class="green--text caption"><fa-icon class="green--text" :icon="['fal', 'check-double']"/> {{$t('tasks.childsDone')}} </span>
+              <span v-if="task.role == 'INITIATOR'" class="red--text caption"><fa-icon class="red--text" :icon="['fal', 'reply']"/> {{$t('tasks.returnedInitiator')}} </span>
+            </div>
           </div>
         </div>
       </div>
-      <div class="justify-end">
-        <!-- {{this.asd(task.dateplan, task.dateFact)}} -->
-        <p class="caption mb-0">{{ $t('tasks.deadline') }}: {{ task.dateplan }}</p>
-        <p class="caption mb-0">{{ $t('tasks.done') }}: {{ task.dateFact }}</p>
+      <div class="task-times">
+        <span v-if="this.taskInProgress()" class="caption mb-0 red--text">{{ task.dateplan }}</span>
+        <span v-if="this.taskIsDone()" class="caption mb-0 green--text"><fa-icon class="green--text" :icon="['fal', 'check']"/> {{ task.dateFact }}</span>
+        <span v-if="this.taskIsDone() && overdue" class="caption mb-0 red--text"><fa-icon class="red--text" :icon="['fal', 'clock']"/> {{ overdue }} {{$t('tasks.days')}}</span>
       </div>
     </div>
   </router-link>
 </template>
 
 <script>
-// import moment from 'moment'
+import moment from 'moment'
 export default {
   name: 'TaskListItem',
   props: {
@@ -53,6 +60,14 @@ export default {
   },
   data: () => ({}),
   computed: {
+    overdue (plan, fact) {
+      var planDate = moment(this.task.dateplan, 'DD.MM.YYYY HH:mm').format()
+      var factDate = moment(this.task.dateFact, 'DD.MM.YYYY HH:mm').format()
+      if (new Date(planDate).getTime() >= new Date(factDate).getTime()) {
+        return false
+      }
+      return moment(factDate).diff(moment(planDate), 'days')
+    },
     cardStyle () {
       return {
         padding: '5px',
@@ -64,11 +79,12 @@ export default {
     }
   },
   methods: {
-    // asd (plan, fact) {
-    //   debugger
-    //   console.log(moment(plan, 'DD.MM.yyyy HH:mm').format())
-    //   return fact
-    // }
+    taskInProgress () {
+      return this.task.status === '' || this.task.status === '*'
+    },
+    taskIsDone () {
+      return this.task.status === '+' || this.task.status === '#'
+    }
   }
 }
 </script>
@@ -86,6 +102,17 @@ export default {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+  }
+  .item-status {
+    display: flex;
+  }
+  .item-status div {
+    padding-right:5px;
+  }
+  .task-times {
+    display: flex;
+    flex-direction: column;
+    text-align: right;
   }
   .row-element {
     display: flex;
