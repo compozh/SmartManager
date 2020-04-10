d7ac253abd62c9e86db468679d952d1e794410e6
Igor Pevchev <pevchev@it-enterprise.com>
2020-04-10 09:19:02 +0300

    sm_v3: no data component updated

--- Applications/SmartManagerV3/src/components/NoData.vue
+++ Applications/SmartManagerV3/src/components/NoData.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="no-data-container">
-    <slot>{{ $t('attachments.noData') }}</slot>
+  <div>
+    <div class="no-data-container noise-bg border-light">
+      <slot>
+        <span class="headline font-weight-light grey--text">
+          {{ $t('attachments.noData') }}
+        </span>
+      </slot>
+    </div>
   </div>
 </template>
 
 <style scoped>
+
   .no-data-container {
     display: flex;
     height: 100%;
@@ -14,4 +21,14 @@
     color: #dae1e7;
     text-align: center;
   }
+  /* TODO: output border-light class to common styles */
+  .border-light {
+    border: 1px solid #e5e5e5;
+    border-radius: 5px;
+  }
+  /* TODO: output noise-bg class to common styles */
+  .noise-bg {
+    background: url('../assets/noise_bg.png');
+  }
+
 </style>
