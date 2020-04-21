70dfdcebc31921d865c0739fd84410b7ad2acf9d
Igor Pevchev <pevchev@it-enterprise.com>
2020-04-21 21:34:46 +0300

    sm: task actions updated

--- Applications/SmartManager/src/store/tasks/actions.js
+++ Applications/SmartManager/src/store/tasks/actions.js
@@ -25,9 +25,10 @@ export default {
         text: i18n.t('notify.taskListError'),
         color: 'error'
       })
+    } finally {
+      commit('SET_LINEAR_PRELOADER', false)
+      commit('STOP_PRELOADER', 'tasks')
     }
-    commit('SET_LINEAR_PRELOADER', false)
-    commit('STOP_PRELOADER', 'tasks')
   },
   async getTaskDetails ({ commit }, { taskId, preLoader }) {
     !preLoader || commit('START_PRELOADER', 'task')
@@ -41,8 +42,9 @@ export default {
         text: i18n.t('notify.taskError'),
         color: 'error'
       })
+    } finally {
+      commit('STOP_PRELOADER', 'task')
     }
-    commit('STOP_PRELOADER', 'task')
   },
   async addComment ({ dispatch, commit }, { comment, params }) {
     const paramsJson = JSON.stringify(params)
@@ -52,7 +54,7 @@ export default {
       if (result.success) {
         await dispatch('getTaskDetails', {
           taskId: params.id,
-          loading: true
+          preLoader: true
         })
       } else {
         commit('SET_NOTIFY', {
@@ -70,15 +72,15 @@ export default {
     }
   },
   async changeTaskStatus ({ dispatch, commit }, params) {
-    const paramsJson = JSON.stringify(params)
+    const statusParams = JSON.stringify(params)
     commit('START_PRELOADER', 'status')
     try {
-      const response = await api.changeTaskStatusInGql(paramsJson)
+      const response = await api.changeTaskStatusInGql(statusParams)
       const result = response.data.smtasksMutation.changeStatus
       if (result.success) {
         await dispatch('getTaskDetails', {
-          id: params.id,
-          loading: true
+          taskId: params.id,
+          preLoader: true
         })
         commit('SET_NOTIFY', {
           text: result.successMessage || i18n.t('notify.statChangeSuccess'),
@@ -99,6 +101,94 @@ export default {
       })
     } finally {
       commit('STOP_PRELOADER', 'status')
+    }
+  },
+  async changeStage ({ dispatch, commit }, params) {
+    const stageParams = JSON.stringify(params)
+    commit('START_PRELOADER', 'stage')
+    try {
+      const response = await api.changeTaskStageInGql(stageParams)
+      const result = response.data.smtasksMutation.changeStage
+      if (result.success) {
+        await dispatch('getTaskDetails', {
+          taskId: params.id,
+          preLoader: true
+        })
+        commit('SET_NOTIFY', {
+          text: result.log || i18n.t('notify.stageChangeSuccess'),
+          color: 'success'
+        })
+      } else {
+        commit('SET_NOTIFY', {
+          text: result.log || i18n.t('notify.stageChangeFail'),
+          color: 'warning'
+        })
+      }
+    } catch (error) {
+      console.error(error.message || error)
+      commit('SET_NOTIFY', {
+        text: error.message || i18n.t('notify.stageChangeError'),
+        color: 'error'
+      })
+    } finally {
+      commit('STOP_PRELOADER', 'stage')
+    }
+  },
+  async taskDelete ({ dispatch, state }, taskId) {
+    // startLoading()
+    try {
+      const response = await api.taskDeleteInGql(taskId)
+      const result = response.data.smtasksMutation.taskDelete
+      // stopLoading()
+      if (result.success) {
+        // If deleted task has parent - update parent
+        const parentId = state.taskInfo[taskId].parentTask.id
+        if (parentId) {
+          await dispatch('getTaskInfo', { taskId: parentId })
+        }
+        // If deleted task bind to case - update case
+        const caseId = state.taskInfo[taskId].caseId
+        if (caseId) {
+          await dispatch('getCaseDetails', { caseId: caseId })
+        }
+        // TODO: If deleted task has child tasks - update child tasks
+        // notify('success', 'taskTitle', 'taskDelSuccess')
+      } else {
+        // notify('warning', 'taskTitle', 'taskDelFail')
+      }
+      return result.success
+    } catch (e) {
+      console.log(e.message)
+      // stopLoading()
+      // notify('danger', 'taskTitle', 'taskDelError')
+      return false
+    }
+  },
+  async taskPin ({ dispatch, commit }, { taskId, pin }) {
+    commit('START_PRELOADER', 'pin')
+    try {
+      const response = await api.taskPinInGql(taskId, pin)
+      const result = response.data.smtasksMutation.taskPin
+      if (result.success) {
+        await dispatch('getTaskDetails', { taskId, preLoader: true })
+        commit('SET_NOTIFY', {
+          text: result.successMessages || i18n.t('notify.taskPinSuccess'),
+          color: 'success'
+        })
+      } else {
+        commit('SET_NOTIFY', {
+          text: result.errorMessage || i18n.t('notify.taskPinFail'),
+          color: 'warning'
+        })
+      }
+    } catch (error) {
+      console.error(error.message || error)
+      commit('SET_NOTIFY', {
+        text: error.message || i18n.t('notify.taskPinError'),
+        color: 'error'
+      })
+    } finally {
+      commit('STOP_PRELOADER', 'pin')
     }
   }
 }
