239f6ff557812286ca4d99735f21b81737772e34
Igor Pevchev <pevchev@it-enterprise.com>
2020-05-05 19:33:12 +0300

    sm: user panel component updated

--- Applications/SmartManager/src/layouts/components/Navbar/UserPanel.vue
+++ Applications/SmartManager/src/layouts/components/Navbar/UserPanel.vue
@@ -1,11 +1,11 @@
 <template>
   <v-menu v-model="menu"
-          close-on-click
+          :close-on-content-click="false"
           offset-y
           transition="scroll-y-transition">
 
     <template v-slot:activator="{ on }">
-      <v-avatar v-on="on" color="primary" class="ml-4" size="32px">
+      <v-avatar v-on="on" color="primary" class="mr-3" size="32px">
         <v-img v-if="userPhoto" :src="userPhoto"/>
         <fa-icon v-else icon="user" inverse/>
       </v-avatar>
@@ -16,14 +16,14 @@
         <v-list-item-icon class="mr-1 align-center">
           <fa-icon icon="user"/>
         </v-list-item-icon>
-        <v-list-item-title>{{ userName }}</v-list-item-title>
+        <v-list-item-title class="body-1">{{ userName }}</v-list-item-title>
       </v-list-item>
 
       <v-list-item>
         <v-list-item-icon class="mr-1 align-center">
           <fa-icon icon="at"/>
         </v-list-item-icon>
-        <v-list-item-title class="caption">{{ userLogin }}</v-list-item-title>
+        <v-list-item-title class="body-2">{{ userLogin }}</v-list-item-title>
       </v-list-item>
 
       <v-divider/>
@@ -31,17 +31,19 @@
       <v-list-group no-action>
 
         <template v-slot:activator>
-          <v-list-item-title>{{ $t('user.delegatedRights') }}</v-list-item-title>
+          <v-list-item-title class="body-1">{{ $t('user.delegatedRights') }}</v-list-item-title>
         </template>
 
         <v-list-item v-for="rights in delegatedRights"
                      :key="rights.ID"
-                     class="mr-2"
+                     class="pl-5"
                      @click="applyDelegatedRights(rights.USERID)">
-          <v-list-item-icon class="mr-1 align-center">
+          <v-list-item-icon class="mr-2 align-center">
             <fa-icon icon="user-tag"/>
           </v-list-item-icon>
-          <v-list-item-title>{{ rights.USERNAME }}</v-list-item-title>
+          <v-list-item-title class="body-1"
+                            :class="{ 'font-weight-bold': rights.IsActive }"
+          >{{ rights.USERNAME }}</v-list-item-title>
         </v-list-item>
 
       </v-list-group>
@@ -52,7 +54,7 @@
         <v-list-item-icon class="mr-1 align-center">
           <fa-icon icon="sign-out-alt"/>
         </v-list-item-icon>
-        <v-list-item-title>{{ $t('buttons.logout') }}</v-list-item-title>
+        <v-list-item-title class="body-1">{{ $t('buttons.logout') }}</v-list-item-title>
       </v-list-item>
 
     </v-list>
