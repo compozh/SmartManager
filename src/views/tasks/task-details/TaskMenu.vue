94e269387e543b9b6ee10f97f772001e8e587612
Ihor Pievchev <pevchev@it-enterprise.com>
2020-05-08 15:19:06 +0300

    task details menu updated

--- src/views/tasks/task-details/TaskMenu.vue
+++ src/views/tasks/task-details/TaskMenu.vue
@@ -9,8 +9,9 @@
           <v-btn v-on="{ ...tooltip, ...menu }"
                  small depressed
                  class="position-absolute"
-                 style="position: absolute; top: 1.9em; right: 1.6em;"
-                 min-width="20">
+                 style="position: absolute; top: 2em; right: 1.6em;"
+                 min-width="20"
+                 :disabled="!menuItems.length">
             <fa-icon icon="ellipsis-v" size="lg"/>
           </v-btn>
         </template>
@@ -20,41 +21,41 @@
     <v-list dense nav>
       <v-list-item v-for="(item, i) in menuItems"
                    :key="i"
-                   @click="$emit(item.action)">
+                   @click="item.action">
         <v-list-item-icon class="ma-0 mr-2 align-self-center align-center">
           <fa-icon :icon="item.icon" :class="item.color + '--text'"/>
         </v-list-item-icon>
-        <v-list-item-title>{{ item.title }}</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-menu>
 </template>
 
 <script>
+import { tasks } from '@/mixins/units'
+
 export default {
   name: 'TaskMenu',
+  mixins: [tasks],
   computed: {
     menuItems () {
-      return [
-        {
-          title: this.$t('buttons.edit'),
-          action: this.$emit('edit'),
-          icon: 'edit',
-          color: 'blue-grey'
-        },
-        {
+      const items = []
+      if (!this.externalTaskCamunda) {
+        items.push({
           title: this.$t('buttons.cancelOff'),
-          action: this.$emit('changeStatus', '-'),
+          action: () => this.$emit('changeStatus', '-'),
           icon: 'horizontal-rule',
           color: 'deep-orange'
-        },
-        {
-          title: this.$t('buttons.delete'),
-          action: 'taskDelete',
-          icon: 'trash',
-          color: 'red'
+        })
+        if (this.taskType !== 'WORKFLOW') {
+          items.push({
+            title: this.$t('buttons.delete'),
+            action: () => this.$emit('taskDelete'),
+            icon: 'trash',
+            color: 'red'
+          })
         }
-      ]
+      }
+      return items
     }
   }
 }
