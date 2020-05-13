db0ac1f6f4a5c16b6cacbdc6e4a89f8e167a1dd9
Ihor Pievchev <pevchev@it-enterprise.com>
2020-05-13 16:15:02 +0300

    edj pdf viewer added

--- src/views/attachments/attachments-viewers/viewers/ejs-pdf-viewer.vue
+++ src/views/attachments/attachments-viewers/viewers/ejs-pdf-viewer.vue
@@ -1,13 +1,46 @@
 <template>
-  $END$
+  <div>
+    <div class="control-section">
+      <ejs-pdfviewer id="pdfviewer"
+                     :serviceUrl="serviceUrl"
+                     :documentPath="documentPath"/>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "ejs-pdf-viewer"
+import Vue from 'vue'
+import { PdfViewerPlugin, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields } from '@syncfusion/ej2-vue-pdfviewer'
+
+Vue.use(PdfViewerPlugin)
+
+export default Vue.extend({
+  props: {
+    url: String
+  },
+  data: () => ({
+    serviceUrl: 'https://ej2services.syncfusion.com/production/web-services/api/pdfviewer',
+    documentPath: 'PDF_Succinctly.pdf'
+  }),
+  provide: {
+    PdfViewer: [Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView, ThumbnailView, Print, TextSelection, TextSearch, Annotation, FormFields]
   }
+})
 </script>
 
 <style scoped>
+
+  @import '~@syncfusion/ej2-base/styles/material.css';
+  @import '~@syncfusion/ej2-buttons/styles/material.css';
+  @import '~@syncfusion/ej2-dropdowns/styles/material.css';
+  @import '~@syncfusion/ej2-inputs/styles/material.css';
+  @import '~@syncfusion/ej2-navigations/styles/material.css';
+  @import '~@syncfusion/ej2-popups/styles/material.css';
+  @import '~@syncfusion/ej2-splitbuttons/styles/material.css';
+  @import "~@syncfusion/ej2-vue-pdfviewer/styles/material.css";
+
+  #pdfviewer {
+    height: 820px;
+  }
 
 </style>
