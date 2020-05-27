cf54c4ac36ce48b9dab1b64c6eebebf3f165b78f
Ihor Pievchev <pevchev@it-enterprise.com>
2020-05-27 20:12:58 +0300

    remove preloader component to app component

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,11 +1,8 @@
 <template>
   <v-app>
-
     <notify/>
-    <pre-loader/>
     <the-side-bar/>
     <the-nav-bar/>
-
     <v-content class="grey lighten-3">
       <v-container fluid style="height: 100%"
                    class="d-flex flex-column pa-2">
@@ -18,7 +15,6 @@
 
 <script>
 import Notify from '@/components/Notify'
-import PreLoader from '@/components/PreLoader'
 import TheNavBar from './components/Navbar/TheNavBar'
 import TheSideBar from './components/Sidebar/TheSideBar'
 import { userInfo, userMethods } from '@/mixins/users'
@@ -29,7 +25,6 @@ export default {
   mixins: [userInfo, userMethods, folders],
   components: {
     Notify,
-    PreLoader,
     TheNavBar,
     TheSideBar
   },
