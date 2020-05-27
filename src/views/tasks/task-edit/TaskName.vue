6bc5f3077264fc39b2f3e11fcfae431c2afb6830
Ihor Pievchev <pevchev@it-enterprise.com>
2020-05-27 20:16:38 +0300

    task name editable component added

---
+++ src/views/tasks/task-edit/TaskName.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-textarea ref="taskNameField"
+              v-model="name"
+              @blur="checkTaskName"
+              :rules="[v => !!v || this.$t('validate.required')]"
+              auto-grow rows="1"
+              solo flat dense
+              class="title font-weight-bold">
+  </v-textarea>
+</template>
+
+<script>
+export default {
+  name: 'TaskName',
+  model: {
+    prop: 'taskName'
+  },
+  props: {
+    taskName: String
+  },
+  data: () => ({
+    initTaskName: ''
+  }),
+  watch: {
+    // Write init task name for backup
+    taskName (name) {
+      if (name.trim() && !this.taskChanged) {
+        this.initTaskName = name
+      }
+    },
+    // Update init task name for backup
+    taskChanged (changed) {
+      if (!changed) {
+        this.initTaskName = this.taskName
+      }
+    }
+  },
+  computed: {
+    name: {
+      get () {
+        return this.taskName
+      },
+      set (name) {
+        if (name !== this.taskName && name !== this.initTaskName) {
+          if (name.trim() && !this.taskChanged) {
+            this.$store.commit('SET_TASK_CHANGED', true)
+          }
+          this.$emit('input', name)
+        }
+      }
+    },
+    taskChanged () {
+      return this.$store.state.tasks.taskChanged
+    }
+  },
+  methods: {
+    checkTaskName () {
+      if (!this.name.trim()) {
+        this.$emit('input', this.initTaskName)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .v-textarea >>> .v-messages__message {
+    font-weight: normal;
+  }
+
+</style>
