3f6bfb60d20352a58d3769cdbce7ebc8c9eee9cd
Ihor Pievchev <pevchev@it-enterprise.com>
2020-06-16 11:47:22 +0300

    cases components created

---
+++ src/views/cases/case-list/CaseCards.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-data-iterator :items="cases"
+                   items-per-page.sync="itemsPerPage"
+                   :page="page"
+                   :search="search"
+                   :hide-default-footer="hideFooter"
+                   :no-data-text="hideNoData ? '' : $t('cases.noCases')"
+                   :footer-props="{itemsPerPageText: $t('cases.perPage')}">
+
+    <template #default="{ items }">
+      <v-row>
+        <v-col v-for="caseItem in items"
+               :key="caseItem.id" cols="12" class="py-1">
+          <case-list-item :caseItem="caseItem"/>
+        </v-col>
+      </v-row>
+    </template>
+
+    <template #no-data v-if="!hideNoData">
+      <div class="primary--text body-2 ml-3 mt-3">{{ $t('cases.noCases') }}</div>
+    </template>
+
+  </v-data-iterator>
+</template>
+
+<script>
+import CaseListItem from '@/views/cases/case-list/CaseListItem.vue'
+
+export default {
+  name: 'CaseCards',
+  components: {
+    CaseListItem
+  },
+  props: {
+    cases: Array,
+    hideNoData: Boolean,
+    hideFooter: Boolean
+  },
+  data: () => ({
+    search: '',
+    page: 1,
+    itemsPerPage: 1
+  }),
+  computed: {
+    numberOfPages () {
+      return Math.ceil(this.items.length / this.itemsPerPage)
+    }
+  },
+  methods: {
+    nextPage () {
+      if (this.page + 1 <= this.numberOfPages) this.page += 1
+    },
+    formerPage () {
+      if (this.page - 1 >= 1) this.page -= 1
+    },
+    updateItemsPerPage (number) {
+      this.itemsPerPage = number
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  /* Set pagination to left */
+  .v-data-iterator >>> .v-data-footer {
+    justify-content: flex-start;
+    font-size: 14px;
+  }
+
+</style>
--- src/views/cases/case-list/CaseList.vue
+++ src/views/cases/case-list/CaseList.vue
@@ -1,16 +1,30 @@
 <template>
-  <no-data/>
+  <v-row no-gutters style="flex-basis: 0;"
+         class="flex-grow-1">
+    <v-col class="pa-0">
+      <v-row dense align-content="start" justify="start">
+        <v-col class="d-flex">
+          <search-field class="mt-n3 ml-1"/>
+          <v-spacer/>
+        </v-col>
+      </v-row>
+      <case-cards :cases="cases"/>
+    </v-col>
+    <router-view/>
+  </v-row>
 </template>
 
 <script>
-import NoData from '@/views/attachments/attachments-viewers/NoData'
+import SearchField from '@/components/SearchField'
+import CaseCards from './CaseCards'
 import { zones, folders, cases } from '@/mixins/units'
 
 export default {
   name: 'CaseList',
   mixins: [zones, folders, cases],
   components: {
-    NoData
+    SearchField,
+    CaseCards
   },
   created () {
     this.getCases()
