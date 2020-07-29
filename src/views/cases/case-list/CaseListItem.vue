a9c886381e3d777e562f5ba12547aa70d63fea01
Ihor Pievchev <pevchev@it-enterprise.com>
2020-07-29 16:59:04 +0300

    case list updated

--- src/views/cases/case-list/CaseListItem.vue
+++ src/views/cases/case-list/CaseListItem.vue
@@ -1,49 +1,47 @@
 <template>
-  <router-link :to="{name: 'case-details', params: {caseId: caseItem.id}}"
-               style="text-decoration: none">
-    <v-card class="pa-2">
-      <div class="item-title">
-        <div class="item-title-desc text-truncate">
-          <div class="column-element">
-            <span class="subtitle-1 task-doc-caption text-truncate d-inline-block ma-0 pl-1">
-              {{ caseItem.name }}</span>
-          </div>
-        </div>
-        <div class="align-center justify-end icons-block">
-          <fa-icon v-show="caseItem.hasComm" class="ml-2" icon="comment-alt-lines"/>
-          <fa-icon v-show="caseItem.hasOrig" class="ml-2 brown--text" icon="paperclip"/>
+  <v-card class="pa-2" ripple
+          :to="{name: 'case-details', params: {caseId: caseItem.id}}">
+    <div class="item-title">
+      <div class="item-title-desc text-truncate">
+        <div class="column-element">
+          <span class="subtitle-1 case-doc-caption text-truncate d-inline-block ma-0 pl-1">
+            {{ caseItem.name }}</span>
         </div>
       </div>
-      <v-divider class="mx-2"/>
-      <div class="item-body">
-        <div class="row-element">
-          <v-avatar color="grey lighten-1" class="mx-3" size="40px">
-            <fa-icon icon="suitcase" inverse/>
-          </v-avatar>
-          <div class="column-element">
-            <span class="body-2 text--secondary">{{ caseItem.fioAdd }}</span>
-            <span class="ma-0 pl-0 subtitle-1 d-flex align-center">
-              <span class=" ma-0 pl-0 pt-1 subtitle-2 text-truncate">{{ caseItem.purpose }}</span>
-            </span>
-          </div>
-        </div>
-        <div class="case-times">
-          <span v-if="caseInProgress"
-                class="body-2 mb-0"
-                :class="overdue ? 'red--text' : 'blue--text'">
-            {{ caseItem.dateTo }}</span>
-          <span v-if="caseIsDone" class="body-2 mb-0 green--text">
-            <fa-icon class="green--text" icon="check"/>
-            {{ caseItem.dateTo }}
-          </span>
-          <span v-if="caseIsDone && overdue" class="body-2 mb-0 red--text">
-            <fa-icon class="red--text" icon="clock"/>
-            {{ overdue }} {{$t('tasks.days')}}
+      <div class="align-center justify-end icons-block">
+        <fa-icon v-show="caseItem.hasComm" class="ml-2" icon="comment-alt-lines"/>
+        <fa-icon v-show="caseItem.hasOrig" class="ml-2 brown--text" icon="paperclip"/>
+      </div>
+    </div>
+    <v-divider class="mx-2"/>
+    <div class="item-body">
+      <div class="row-element">
+        <v-avatar color="grey lighten-1" class="mx-3" size="40px">
+          <fa-icon icon="suitcase" inverse/>
+        </v-avatar>
+        <div class="column-element">
+          <span class="body-2 text--secondary">{{ caseItem.fioAdd }}</span>
+          <span class="ma-0 pl-0 subtitle-1 d-flex align-center">
+            <span class=" ma-0 pl-0 pt-1 subtitle-2 text-truncate">{{ caseItem.purpose }}</span>
           </span>
         </div>
       </div>
-    </v-card>
-  </router-link>
+      <div class="case-times">
+        <span v-if="caseInProgress"
+              class="body-2 mb-0"
+              :class="overdue ? 'red--text' : 'blue--text'">
+          {{ caseItem.dateTo }}</span>
+        <span v-if="caseIsDone" class="body-2 mb-0 green--text">
+          <fa-icon class="green--text" icon="check"/>
+          {{ caseItem.dateTo }}
+        </span>
+        <span v-if="caseIsDone && overdue" class="body-2 mb-0 red--text">
+          <fa-icon class="red--text" icon="clock"/>
+          {{ overdue }} {{$t('tasks.days')}}
+        </span>
+      </div>
+    </div>
+  </v-card>
 </template>
 
 <script>
@@ -80,7 +78,7 @@ export default {
     justify-content: space-between;
   }
 
-  .task-doc-caption {
+  .case-doc-caption {
     line-height: 18px;
   }
 
