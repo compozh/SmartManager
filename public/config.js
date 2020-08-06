9fe7405410634c72646f2105c1b5eff67a6279d8
Melentiev Yevhen <melentyev@it.ua>
2020-08-06 14:24:08 +0300

    fix url

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
-window.devMode = false
+window.devMode = true
 var DM = window.DOCKERMODE
 window.appConfig = {
-  GrapgQlUrl: DM ? '*GRAPHQLURL*' : (window.devMode ? 'http://localhost:5002/' : 'https://m.it.ua/GraphQlServer/'),
+  GrapgQlUrl: DM ? '*GRAPHQLURL*' : (window.devMode ? 'http://localhost:5000/' : 'https://m.it.ua/GraphQlServer/'),
   BASE_URL: window.VUE_APP_BASE_PATH,
-  WsUrl: window.devMode ? 'http://localhost:64137/ws/api/' : '/ws/api/',
+  WsUrl: window.devMode ? 'http://localhost:64137/ws' : '/ws',
   SignalRUrl: DM ? '*SIGNALRURL*' : 'https://m.it.ua/ws/signalr/hubs'
 }
