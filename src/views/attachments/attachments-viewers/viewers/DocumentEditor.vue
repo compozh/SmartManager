46b8618aee77dc76463a6338810b3b54237be319
Ihor Pievchev <pevchev@it-enterprise.com>
2020-09-11 16:53:12 +0300

    document viewer component created

--- src/views/attachments/attachments-viewers/viewers/DocumentEditor.vue
+++ src/views/attachments/attachments-viewers/viewers/DocumentEditor.vue
@@ -1,13 +1,30 @@
 <template>
-  $END$
+  <ejs-documenteditorcontainer ref='documenteditor' :serviceUrl='serviceUrl' style='height:800px;' id='container' :enableToolbar='true'></ejs-documenteditorcontainer>
 </template>
 
 <script>
-  export default {
-    name: "DocumentEditor"
+import Vue from 'vue'
+import { DocumentEditorContainerPlugin, Toolbar } from '@syncfusion/ej2-vue-documenteditor'
+
+Vue.use(DocumentEditorContainerPlugin)
+export default {
+  data: () => ({
+    serviceUrl: 'https://ej2services.syncfusion.com/production/web-services/api/documenteditor/'
+  }),
+  provide: {
+    DocumentEditorContainer: [Toolbar]
   }
+}
 </script>
 
 <style scoped>
-
+  @import '~@syncfusion/ej2-base/styles/material.css';
+  @import '~@syncfusion/ej2-buttons/styles/material.css';
+  @import '~@syncfusion/ej2-inputs/styles/material.css';
+  @import '~@syncfusion/ej2-popups/styles/material.css';
+  @import '~@syncfusion/ej2-lists/styles/material.css';
+  @import '~@syncfusion/ej2-navigations/styles/material.css';
+  @import '~@syncfusion/ej2-splitbuttons/styles/material.css';
+  @import '~@syncfusion/ej2-dropdowns/styles/material.css';
+  @import "~@syncfusion/ej2-vue-documenteditor/styles/material.css";
 </style>
