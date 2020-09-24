b30244454b84849a11d169bc08a009954eba15a0
Ihor Pievchev <pevchev@it-enterprise.com>
2020-09-24 17:56:57 +0300

    DROP_TASK_IN_FOLDER & CHANGE_TASK_COUNT mutations created

--- src/store/folders/mutations.js
+++ src/store/folders/mutations.js
@@ -4,5 +4,10 @@ export default {
   },
   SET_ACTIVE_FOLDER (state, { folderId }) {
     state.activeFolderId = folderId
+  },
+  CHANGE_TASKS_COUNT (state, folderId) {
+    const taskFolders = state.folderGroups.taskFolders
+    const folder = taskFolders.find(folder => folder.Code === folderId)
+    if (folder) folder.Count -= 1
   }
 }
