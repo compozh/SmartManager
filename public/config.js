62961bdb83507255790dbfb4902e5d4a65a9796f
Ihor Pievchev <pevchev@it-enterprise.com>
2020-10-16 15:10:20 +0300

    added "SiteUrl" prop to configs

--- public/config.js
+++ public/config.js
@@ -4,5 +4,6 @@ window.appConfig = {
   GrapgQlUrl: DM ? '*GRAPHQLURL*' : (window.devMode ? 'http://localhost:5002/' : 'https://m.it.ua/GraphQlServer/'),
   BASE_URL: window.VUE_APP_BASE_PATH,
   WsUrl: window.devMode ? 'http://localhost:64137/ws' : '/ws',
-  SignalRUrl: DM ? '*SIGNALRURL*' : 'https://m.it.ua/ws/signalr/hubs'
+  SignalRUrl: DM ? '*SIGNALRURL*' : 'https://m.it.ua/ws/signalr/hubs',
+  SiteUrl: DM ? '*SITEURL*' : 'https://m.it.ua'
 }
