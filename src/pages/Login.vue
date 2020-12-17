48a4c0d100b0208a7e5a44a5c17171b469b09776
Ihor Pievchev <pevchev@it-enterprise.com>
2020-12-17 16:40:22 +0200

    update(smartId): add smartId button

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,8 +2,8 @@
   <v-container fluid class="fill-height justify-center">
     <v-row align="center" justify="center">
       <v-col cols="10" sm="7" md="5" lg="4" xl="3">
-      <v-card>
-        <v-row class="pa-5" align="center">
+      <v-card class="pa-5">
+        <v-row align="center">
           <v-col class="d-flex flex-column">
             <h3 class="mb-4">{{ $t('login.title') }}</h3>
             <p>{{ $t('login.welcome') }}</p>
@@ -40,6 +40,8 @@
             <div class="d-flex align-center justify-space-between">
               <v-checkbox :label="$t('login.rememberMe')"
                           v-model="rememberMe"/>
+              <v-spacer></v-spacer>
+              <div ref="smartId" class="btn-smart-id" v-ripple></div>
               <v-btn color="primary" @click="loginMethod">
                 {{ $t('buttons.login') }}
               </v-btn>
@@ -61,15 +63,18 @@ import { userMethods } from '@/mixins/users'
 export default {
   name: 'Login',
   mixins: [userMethods],
-  created () {
-    this.logout()
-    this.getAuthTypes()
-  },
   computed: {
     forgotPasswordUrl () {
       const forgotPasswordUrl = '/webparts/?id=LOGIN_REQUESTRECOVERPASSWORD&devexpress_theme=moderno&itLanguage='
       return window.appConfig.SiteUrl + forgotPasswordUrl + this.$i18n.locale
     }
+  },
+  created () {
+    this.logout()
+    this.getAuthTypes()
+  },
+  mounted () {
+    this.$refs.smartId.addEventListener('click', this.smartId)
   },
   methods: {
     async getAuthTypes () {
@@ -78,3 +83,21 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+  .btn-smart-id {
+    background: url("../assets/smart_id_btn_bg.png") no-repeat center center;
+    background-color: #e6e6e6;
+    background-size: 85%;
+    border-radius: 4px;
+    height: 36px;
+    width: 76px;
+    margin-right: 0.75rem;
+    cursor: pointer;
+    box-shadow: 0 1px 1px -2px rgba(0,0,0,.2),
+                0 2px 2px 0 rgba(0,0,0,.14),
+                0 1px 5px 0 rgba(0,0,0,.12)!important;
+  }
+
+</style>
