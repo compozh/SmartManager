b347147d015c7a6770eeafaeeb9e89e30f6ec66f
Ihor Pievchev <pevchev@it-enterprise.com>
2021-01-25 14:59:18 +0200

    create(eds): add new components

---
+++ src/views/attachments/attachment-eds/eds-create/EdsFileKey.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <!-- File key input -->
+    <v-file-input :value="fileKey"
+                  @change="$emit('input', $event)"
+                  :label="$t('eds.privateKey')"
+                  :placeholder="$t('eds.selectKey')"
+                  outlined dense
+                  prepend-icon=""
+                  :clearable="false"
+                  :show-size="1000"
+                  :disabled="loading"
+                  autocomplete="off"
+                  class="body-1 clearable">
+      <template #prepend-inner>
+        <v-icon small class="pt-1 pr-2">fas fa-paperclip</v-icon>
+      </template>
+      <template #selection="{ text }">
+        <v-chip label small color="primary">{{ text }}</v-chip>
+      </template>
+      <template #append>
+        <v-btn icon small class="clear-btn"
+               @click="$emit('input', null)">
+          <fa-icon icon="times" type="fal" size="lg"/>
+        </v-btn>
+      </template>
+    </v-file-input>
+
+    <!-- Key password input -->
+    <v-text-field :value="password"
+                  @input="$emit('update:password', $event)"
+                  :label="$t('login.password')"
+                  :placeholder="$t('login.passwordPlaceholder')"
+                  outlined dense
+                  name="password"
+                  type="password"
+                  hide-details
+                  :disabled="loading"
+                  autocomplete="off"
+                  @keyup.enter="$emit('read-key')"
+                  class="body-1 clearable">
+      <template #prepend-inner>
+        <v-icon small class="pt-1 pr-2">fas fa-unlock</v-icon>
+      </template>
+      <template #append>
+        <v-btn icon small class="clear-btn"
+               @click="$emit('update:password', null)">
+          <fa-icon icon="times" type="fal" size="lg"/>
+        </v-btn>
+      </template>
+    </v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EdsFileKey',
+  model: {
+    prop: 'fileKey'
+  },
+  props: {
+    fileKey: [File, null],
+    password: [String, null],
+    loading: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/attachments/attachment-eds/eds-create/EdsHardwareKey.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- Key password input -->
+    <v-text-field v-model="password"
+                  :label="$t('login.password')"
+                  :placeholder="$t('login.passwordPlaceholder')"
+                  outlined dense
+                  name="password"
+                  type="password"
+                  hide-details
+                  :disabled="loading"
+                  autocomplete="off"
+                  @keyup.enter="readPrivateKey"
+                  class="body-1 clearable">
+      <template #prepend-inner>
+        <v-icon small class="pt-1 pr-2">fas fa-unlock</v-icon>
+      </template>
+      <template #append>
+        <v-btn icon small class="clear-btn"
+               @click="password = null">
+          <fa-icon icon="times" type="fal" size="lg"/>
+        </v-btn>
+      </template>
+    </v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EdsFileKey',
+  props: ['signType']
+}
+</script>
+
+<style scoped>
+
+</style>
