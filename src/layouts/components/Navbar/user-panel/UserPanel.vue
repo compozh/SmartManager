44e853afbd7597f9acaa687bb1ec65c50766d63a
Ihor Pievchev <pevchev@it-enterprise.com>
2021-01-25 15:02:50 +0200

    update(userMenu): add clear key item

--- src/layouts/components/Navbar/user-panel/UserPanel.vue
+++ src/layouts/components/Navbar/user-panel/UserPanel.vue
@@ -30,6 +30,14 @@
         <v-list-item-title class="body-2">{{ userLogin }}</v-list-item-title>
       </v-list-item>
 
+      <v-list-item v-if="isPrivateKey"
+                   @click="window.ds.resetPrivateKey()">
+        <v-list-item-icon class="mr-2 align-center">
+          <fa-icon icon="at"/>
+        </v-list-item-icon>
+        <v-list-item-title class="body-1" v-text="$t('eds.clearKey')"/>
+      </v-list-item>
+
       <v-divider/>
 
       <v-list-item v-if="delegatedUsers.length"
@@ -74,15 +82,22 @@
         <v-list-item-title class="body-1">{{ $t('buttons.logout') }}</v-list-item-title>
       </v-list-item>
     </v-list>
-    <delegated-users v-model="usersDialog"/>
-    <delegated-rights v-model="rightsDialog"
+
+    <!-- Delegated users dialog -->
+    <delegated-users v-if="usersDialog" v-model="usersDialog"/>
+
+    <!-- Delegated rights dialog -->
+    <delegated-rights v-if="rightsDialog" v-model="rightsDialog"
                       @add-delegation="delegateDialog = true"/>
-    <delegate-user v-model="delegateDialog"/>
+
+    <!-- Delegate rights to user dialog -->
+    <delegate-user v-if="delegateDialog" v-model="delegateDialog"/>
   </v-menu>
 </template>
 
 <script>
 import { userInfo, userMethods } from '@/mixins/users'
+import eds from '@/mixins/eds'
 
 const DelegatedUsers = () => import('./DelegatedUsers')
 const DelegatedRights = () => import('./DelegatedRights')
@@ -90,7 +105,7 @@ const DelegateUser = () => import('./DelegateUser')
 
 export default {
   name: 'UserPanel',
-  mixins: [userInfo, userMethods],
+  mixins: [userInfo, userMethods, eds],
   components: {
     DelegatedUsers,
     DelegatedRights,
