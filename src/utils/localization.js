2776a8a9fe771cadb7abbca1d6459bb1cbabc9a3
Ihor Pievchev <pevchev@it-enterprise.com>
2021-02-16 15:55:33 +0200

    create(localization): chinese localization file added

--- src/utils/localization.js
+++ src/utils/localization.js
@@ -4,6 +4,7 @@ import Localization from '@it-enterprise/localization'
 
 Vue.use(Localization, { dependencies: { i18n } })
 
-Vue.prototype.$localization.RegisterLanguage('', 'ru', () => import('@/i18n/locales/ru.json'))
-Vue.prototype.$localization.RegisterLanguage('', 'en', () => import('@/i18n/locales/en.json'))
 Vue.prototype.$localization.RegisterLanguage('', 'uk', () => import('@/i18n/locales/uk.json'))
+Vue.prototype.$localization.RegisterLanguage('', 'en', () => import('@/i18n/locales/en.json'))
+Vue.prototype.$localization.RegisterLanguage('', 'zh', () => import('@/i18n/locales/cn.json'))
+Vue.prototype.$localization.RegisterLanguage('', 'ru', () => import('@/i18n/locales/ru.json'))
