9100699a7be44ecfd53be2fb4474adfdda067a5c
Ihor Pievchev <pevchev@it-enterprise.com>
2021-03-03 12:45:03 +0200

    update(langs): add language switcher according lang param

--- src/layouts/components/Navbar/LangSwitcher.vue
+++ src/layouts/components/Navbar/LangSwitcher.vue
@@ -4,7 +4,7 @@
           offset-y
           transition="scroll-y-transition">
 
-    <template v-slot:activator="{ on }">
+    <template #activator="{ on }">
       <v-btn icon small v-on="on">
         <span class="body-1">{{ lang.flag }}</span>
       </v-btn>
@@ -30,7 +30,7 @@ import { folders } from '@/mixins/units'
 export default {
   name: 'LangSwitcher',
   data: () => ({
-    locales: [
+    localesList: [
       { code: 'uk', name: 'Українська', flag: 'ua' },
       { code: 'en', name: 'English', flag: 'en' },
       { code: 'zh', name: 'Chinese', flag: 'zh' },
@@ -41,25 +41,35 @@ export default {
   computed: {
     lang: {
       get () {
-        return this.locales
+        let locale = this.locales
           .find(lang => lang.code === this.$i18n.locale)
+        if (locale) return locale
+        locale = this.locales[0]
+        this.setLang(locale.code)
+        return locale
       },
       set (lang) {
         this.setLang(lang)
-        // Перечитка папок для обновления локализации
-        this.getFolders()
       }
+    },
+
+    locales () {
+      if (window.appConfig.lang) {
+        const langs = window.appConfig.lang.split(',')
+        return this.localesList.filter(locale => {
+          return langs.some(lang => lang.trim() === locale.code)
+        })
+      }
+      return this.localesList
     }
   },
   methods: {
     setLang (lang) {
       this.$vuetify.lang.current = lang
       this.$localization.SetLocalization(lang)
+      // Перечитка папок для обновления локализации
+      this.getFolders()
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
