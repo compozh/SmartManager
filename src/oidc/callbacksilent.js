4005d0f55d146ff975b23153c3d4bbbf864a7b48
Ihor Pievchev <pevchev@it-enterprise.com>
2021-03-09 20:25:35 +0200

    remove(oidc): delete unused folders

--- src/oidc/callbacksilent.js
+++
@@ -1,8 +0,0 @@
-import Oidc from 'oidc-client'
-
-Oidc.Log.logger = console
-Oidc.Log.level = Oidc.Log.INFO
-new Oidc.UserManager().signinSilentCallback()
-  .catch((err) => {
-    console.warn('[OIDC CALLBACK]', err)
-  })
