eab6de1a1b0fef112ff005041fbd7dcd0f810873
Ihor Pievchev <pevchev@it-enterprise.com>
2021-03-09 19:20:22 +0200

    remove(app): delete unused files

--- docker.ps1
+++
@@ -1,6 +0,0 @@
-docker build . -t itenterprise.azurecr.io/it/smartmanager
-docker push  itenterprise.azurecr.io/it/smartmanager
-helm delete  smartmanager
-helm install smartmanager ./chart
-Start-Sleep -s 10
-kubectl get pods -l release=smartmanager
